5da9944dc83540be5dbab46cf23e852e6b4a489a
Luis Rodrigo <NiziulLuizin@Gmail.com>
2020-12-13 22:01:39 -0300

    Adicionando Exercício014

---
+++ Cursos/JAVASCRIPT/Exercicíos/Exercicio014/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+console.log("Tudo bem?")
+*/
+
+/*
+var c = 1
+while (c <= 500) {
+    console.log(`Tudo bem? ${c}`)
+    c++
+}
+*/
+
+var c = 1
+do  {
+    console.log(`Tudo bem? ${c}`)
+    c++
+} while (c <= 5)
