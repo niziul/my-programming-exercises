15ee70eeba777f27d948bf8a7d43026fd36214a6
Luis Rodrigo <NiziulLuizin@Gmail.com>
2020-12-21 12:45:58 -0300

    commit

--- JAVASCRIPT/PraticandoJS/Pratica004/script.js
+++ JAVASCRIPT/PraticandoJS/Pratica004/script.js
@@ -8,9 +8,10 @@ function MostraTabuada(){
         let n = Number(num.value)
         let c = 1
         tab.innerHTML = ''
-        while (c < 11) {
+        while (c < 10) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n * c}`
+            item.value = 'tab${c}'
             tab.appendChild(item)
             c++
         }
