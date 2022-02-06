e3c5dbaad7934c7f69bb1e36bccb0213551ed25e
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-06 20:48:02 -0300

    Analyzing How Debugging Works

--- FundamentosDoC#/MeuApp/Program.cs
+++ FundamentosDoC#/MeuApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MeuApp
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var text = "Testando";
+            Console.WriteLine(text);
+        }
+    }
+}
+
