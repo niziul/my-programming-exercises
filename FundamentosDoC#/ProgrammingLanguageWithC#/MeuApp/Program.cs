f49f0b5f230a96177d9f2c68d9bbfa4b9c1d366d
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-07 22:57:09 -0300

    Visualizing the behavior of namespaces

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "Testing";
+
+            interfaceLineChar();
+            interfaceForShowUser();
+            Console.WriteLine(text.ToLower());  
+            interfaceForShowUser();
+            interfaceLineChar();
+
+            void interfaceLineChar()
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.Write("-");
+                }
+            }
+            void interfaceForShowUser()
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.WriteLine("-");
+                }
+            }
+        }
+
+        
+    }
+}
