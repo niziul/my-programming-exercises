abf1fb0fe880f1d4b49eec69fa5dcf0fe97ff5ec
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-08 11:40:34 -0300

    Learning about comments

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppComments/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+    Exemplos de cometários descritivos, embora não necessários:
+*/
+
+// Importações
+using System;
+
+// Namespace
+namespace MeuAppComments
+{
+    // Classe principal do programa
+
+    // Anotações XML:
+    
+    /// <properties>
+    ///
+    class Program
+    {
+        // Função principal do programa
+        static void Main(string[] args)
+        {
+            /*
+                Usando as teclas de atalho: "Ctrl" + "K" + "C" é possível realizar um cometário de uma única linha.
+                Para desfazer o cometário utilize as teclas "Ctrl" + "K" + "U"
+            */
+
+            // var text = "Testing"; 
+
+
+
+            var text = "Testing";
+            Console.WriteLine(text.ToLower());          
+        }       
+    }
+}
