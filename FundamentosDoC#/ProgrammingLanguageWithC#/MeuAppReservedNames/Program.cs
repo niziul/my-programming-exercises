1e62c8ca460810e8736851e0b0c78c05b601e1bb
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-08 11:16:57 -0300

    Learning about Reserved Names

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppReservedNames/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MeuAppReservedNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int internal = 25; // <-- "internal" é uma palavra reservada em c#
+            
+            var text = "Testing";
+            Console.WriteLine(text.ToLower());          
+        }       
+    }
+}
