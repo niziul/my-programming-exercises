c111cb003b176f771a24f7cd8eb53e73afb00b56
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-08 11:00:20 -0300

    Correction of the project about the "Learning more about variables" lesson

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppVariables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade; // Correto inicia com ZERO
+            //int idade = 25; // Correto inicia com 25
+            //var idade = 25; // Correto inicia com 25
+            //var idade; // Errado
+
+
+            var text = "Testing";
+            Console.WriteLine(text.ToLower());          
+        }       
+    }
+}
