d63d9f36486810a82460b91d592364b8ecf68fd1
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-08 11:06:16 -0300

    Learning more about constants

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppConstants/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MeuAppConstants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //const int IDADE_MINIMA; // Correto inicia com ZERO
+            const int IDADE_MINIMA = 25; // Correto inica com 25
+            //const int IDADE_MINIMA = 25; // Errado
+            //const int IDADE_MINIMA; // Errado
+
+
+            var text = "Testing";
+            Console.WriteLine(text.ToLower());          
+        }       
+    }
+}
