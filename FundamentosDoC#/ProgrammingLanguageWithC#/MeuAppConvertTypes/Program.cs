e06c5a849be338012c9d87f90cdfa565375986d1
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-09 14:14:38 -0300

    Learning to use conversions

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppConvertTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MeuAppConvertTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inteiro = 100;
+            float real = 25.5f;
+            
+            /*
+                Conversão implícita:
+            */
+
+            // inteiro = real; // <-- incorreto
+            // real = inteiro; // <-- correto
+
+            /*
+                conversão explicita:
+            */
+
+            // inteiro = (int)real; 
+            // Console.WriteLine(inteiro);
+            
+            // string valorReal = real.ToString();
+            // inteiro = int.Parse("255");
+            // Console.WriteLine(valorReal);
+
+            inteiro = Convert.ToInt32(real);
+            Console.WriteLine(inteiro);
+            Console.WriteLine(Convert.ToBoolean(1));
+        }
+    }
+}
