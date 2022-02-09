b7c73f819c8edec2e715ada0d0d00d2d31d85558
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-08 23:14:37 -0300

    Learning about real numbers

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppRealNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MeuAppRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var salario = 2500;
+            var salarioDouble = 2.500;           
+            
+            float salarioFloat = 2.500f;
+            double salarioSemestralDouble = 22.000;
+            decimal salarioDecimal = 25.000m;
+        }
+    }
+}
