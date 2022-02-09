1066d832401b4b21f2fff7711351cb673ad876c7
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-09 10:54:01 -0300

    Understanding the use of Object

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppObject/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MeuAppObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object quantidade;
+
+            quantidade = 2.5;
+            quantidade = 1;
+            quantidade = "Teste";
+        }
+    }
+}
