9c85e460decf43021727ea8af93eb2a83ee5cd78
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-09 19:26:56 -0300

    Learning how to create and manage methods and functions

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppMethodsAndFunctions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MeuAppMethodsAndFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeuMetodo();
+        }
+
+        static void MeuMetodo()
+        {
+            Console.WriteLine(RetornaNome("Niziul", "luizin"));
+        }
+
+
+        static string RetornaNome(
+            string nome, 
+            string sobrenome,
+            /*
+                Parâmetros opcionais. (Regras: tem que sempre ser atribuído, e se adicionado por ultimo) :
+            */
+            int idade = 20,   
+            bool teste = false,
+            double novo = 20.32
+        )
+        {
+            return nome + " " + sobrenome + " " + idade.ToString();
+        }
+        
+    }
+}
