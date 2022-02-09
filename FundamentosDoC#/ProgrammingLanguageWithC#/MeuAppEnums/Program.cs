3398c54bf62af76df5f4a18be39e05f7f0b1ab65
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-09 20:58:40 -0300

    Learning how to create and use enums

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppEnums/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MeuAppEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product mouse = new Product(1, "Mouse Gamer", 200.32, EProductType.Product);
+            var manutencaoEletrica = new Product(2, "Manutenção elétrica residencial", 500, EProductType.Service);
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+            Console.WriteLine(mouse.Type);
+            Console.WriteLine(mouse.PriceInDolar(5.50).ToString("F"));
+            
+            Console.WriteLine();
+
+            Console.WriteLine(manutencaoEletrica.Id);
+            Console.WriteLine(manutencaoEletrica.Name);
+            Console.WriteLine(manutencaoEletrica.Price);
+            Console.WriteLine(manutencaoEletrica.Type);
+            Console.WriteLine((int)manutencaoEletrica.Type);
+            Console.WriteLine(manutencaoEletrica.PriceInDolar(5.50).ToString("F"));
+        }
+    }
+
+    struct Product
+    {
+        public Product(int id, string name, double price, EProductType type)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Type = type;
+        }
+        
+
+        public int Id;
+        public string Name;
+        public double Price;
+        public EProductType Type;
+
+        public double PriceInDolar(double dolar)
+        {
+            return Price * dolar;
+        }
+    }
+    enum EProductType
+    {
+        Product = 1,
+        Service = 2
+    }
+}
