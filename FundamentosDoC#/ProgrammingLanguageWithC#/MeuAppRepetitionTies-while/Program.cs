76fbcde9bfef9757b40e4d392f09c75bbcb4b388
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-09 18:40:58 -0300

    Understand how "while" repetition loops work

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppRepetitionTies-while/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MeuAppRepetitionTiesWhile
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            // var valor = 0;
+
+            /*
+                Conta de 0 a 5 
+                Exibe de 0 a 5 na tela
+            */
+            // while (valor <= 5)
+            // {
+            //     Console.WriteLine(valor);
+            //     valor++;
+            // }
+
+
+
+
+
+            // var valor = 0;
+
+            /*
+                Conta de 0 a 5 
+                Exibe de 1 a 6 na tela
+            */
+            // while (valor <= 5)
+            // {
+            //     valor++;
+            //     Console.WriteLine(valor);
+            // }
+            
+
+
+
+
+
+            var valor = 0;
+
+            // Infinito
+            while (true)
+            {
+                valor++;
+                Console.Clear();
+                Console.WriteLine(valor);
+            }
+            
+
+
+        }
+    }
+}
