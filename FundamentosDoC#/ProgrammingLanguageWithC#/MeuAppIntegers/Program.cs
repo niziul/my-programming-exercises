97de7238fff723d97163baffd71bb935b3a885ba
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-08 23:04:49 -0300

    Learning about integers

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppIntegers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MeuAppIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short idadeShort = 25;
+            //ushort idadeUshort = -25;
+            int idadeInt = 25;
+            //uint idadeUint = -25;
+            long idadeLong = 25;
+            //ulong idadeUlong = -25;
+        }
+    }
+}
