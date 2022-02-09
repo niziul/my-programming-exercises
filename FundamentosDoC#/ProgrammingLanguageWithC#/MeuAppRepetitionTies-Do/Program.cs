3702ece0cc39110d77eec7e429a04baf3f7c9ae3
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-09 18:47:25 -0300

    Understand how "Do while" repeating loops work

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppRepetitionTies-Do/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MeuAppRepetitionTiesDo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+
+            do 
+            {
+                Console.WriteLine(valor);
+                valor++;
+            } while (valor <= 5);
+        }
+    }
+}
