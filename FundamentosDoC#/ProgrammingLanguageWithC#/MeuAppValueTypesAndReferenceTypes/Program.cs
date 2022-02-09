b42b2eebf6498044408ecc4e4914786b1eaffe56
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-09 19:58:59 -0300

    Learning more about Value Types and Reference Types

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppValueTypesAndReferenceTypes/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MeuAppValueTypesAndReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type Stack:");
+            /*
+                Type Stack:
+            */
+
+            int x = 25;
+            int y = x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+
+            Console.WriteLine();
+
+            x = 32;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+
+            Console.WriteLine();
+
+            Console.WriteLine("Type Heap:");
+            /*
+                Type Heap:
+            */
+            
+            var arr = new string[2];
+            arr[0] = "Item 1";
+
+            var arr2 = arr;
+
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
+
+
+            Console.WriteLine();   
+            
+            arr[0] = "Item 2";
+
+
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
+
+            
+        }
+    }
+}
