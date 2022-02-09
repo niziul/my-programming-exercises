77987f8e62f76b6b97e6b201c8bee00037300c4a
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-09 15:16:18 -0300

    Learning how operators work (Arithmetic, Assignment and Comparison)

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppUsingOperators/Program.cs
@@ -0,0 +1,275 @@
+﻿using System;
+
+namespace MeuAppUsingOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///
+            ///   Operadores aritméticos:
+            ///
+
+            // int x = 2;
+
+            // x = 2 + 2; // <-- Soma
+            // Console.WriteLine(x);
+            // x = 2 / 2; // <-- Divisão
+            // Console.WriteLine(x);
+
+
+
+            /*
+                input: 3
+            */            
+
+            // double x = 2; // Mais precisão!
+
+            // x = 75 / 23.8;
+            // Console.WriteLine(x);
+
+
+
+
+            /*
+                input: 3,1512605042016806
+            */            
+            // double x = 2; 
+            
+            // x = 75 * 23.8;
+            // Console.WriteLine(x);
+
+
+
+
+            /*
+                input: 1785
+            */
+            // double x = 2;
+            
+            // x = 75 - 23.8;
+            // Console.WriteLine(x);
+
+
+
+
+            /*
+                input: 6
+            */
+            // double x = 2;
+            
+            // x = 2 + 2 * 2;
+            // Console.WriteLine(x);
+
+            
+
+            /*
+                input: 6
+            */
+            // double x = 2;
+            
+            // x = 2 + (2 * 2);
+            // Console.WriteLine(x);
+
+
+ 
+            /*
+                input: 8
+            */
+            // double x = 2;
+            
+            // x = (2 + 2) * 2;
+            // Console.WriteLine(x);
+
+
+
+            ///
+            ///   Operadores de atribuição:
+            ///
+
+            /*
+                input: 5
+            */
+            // int x = 2;
+            
+            // x += 3; // <-- x = x + 3;
+            // Console.WriteLine(x);
+            
+
+
+            
+            /*
+                input: -1
+            */
+            // int x = 2;
+            
+            // x -= 3; // <-- x = x - 3;
+            // Console.WriteLine(x);
+
+
+
+            /*
+                input: 6
+            */
+            // int x = 2;
+            
+            // x *= 3; // <-- x = x * 3;
+            // Console.WriteLine(x);
+
+
+
+
+            /*
+                input: 0
+            */
+            // int x = 2;
+            
+            // x /= 3; // <-- x = x / 3;
+            // Console.WriteLine(x);
+
+
+
+
+            /*
+                input: 3
+            */
+            // int x = 2;
+            
+            // x++; // <-- x += x;
+            // Console.WriteLine(x);
+
+
+
+
+            /*
+                input: 1
+            */
+            // int x = 2;
+            
+            // x--; // <-- x -= x;
+            // Console.WriteLine(x);
+
+
+
+
+
+
+            ///
+            ///   Operadores de comparação:
+            ///
+
+            /*
+                input: False
+            */
+            // int x = 2;
+            
+            // Console.WriteLine(x == 5);
+
+
+
+
+            /*
+                input: True
+            */
+            // int x = 2;
+            
+            // Console.WriteLine(x != 5);
+
+
+
+            /*
+                input: False
+            */
+            // int x = 2;
+            
+            // Console.WriteLine(x > 5);
+
+
+            /*
+                input: True
+            */
+            // int x = 2;
+            
+            // Console.WriteLine(x < 5);
+            
+
+
+
+            /*
+                input: False
+            */
+            // int x = 2;
+            
+            // Console.WriteLine(x > 5);
+
+
+
+
+
+            /*
+                input: False
+            */
+            // int x = 2;
+            
+            // Console.WriteLine(x >= 5);
+
+
+
+
+
+            /*
+                input: True
+            */
+            // int x = 2;
+            
+            // Console.WriteLine(x <= 5);
+
+
+
+
+
+            /*
+                input: True
+            */
+            // int x = 25;
+            
+            // Console.WriteLine(x >= 25);
+
+
+
+
+            
+            /*
+                input: True
+            */
+            // int x = 25;
+             
+            // Console.WriteLine(x <= 25);
+
+
+
+
+
+            /*
+                input: False
+            */
+            // int x = 25;
+             
+            // Console.WriteLine(x > 25);
+
+
+
+
+
+            /*
+                input: False
+            */
+            int x = 25;
+             
+            Console.WriteLine(x < 25);
+
+
+
+        }
+    }
+}
