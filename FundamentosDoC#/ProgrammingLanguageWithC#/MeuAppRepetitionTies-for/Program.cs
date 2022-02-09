0c5456d3197fd0782685a5b9cb45e2ebebf99a62
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-09 18:23:47 -0300

    Understanding how "for" repetition loops work

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppRepetitionTies-for/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MeuAppRepetitionTiesFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Conta de 0 a 5 
+            for (var i = 0; i <= 5; i++)
+                Console.WriteLine(i);
+
+            Console.WriteLine();    
+
+            // Conta de 1 a 5 
+            for (var i = 1; i <= 5; i++)
+                Console.WriteLine(i);
+                
+            Console.WriteLine();    
+
+            // Conta de 0 a 4 
+            for (var i = 0; i < 5; i++)
+                Console.WriteLine(i);
+
+            Console.WriteLine();    
+
+            // Conta de 5 a 0 
+            for (var i = 5; i >= 0; i--)
+                Console.WriteLine(i);
+             
+        }
+    }
+}
