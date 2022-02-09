d8c67c1e4709754b514f63343dedbaabdb2d899d
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-08 23:18:11 -0300

    Learning about "Boolean

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppBoolean/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MeuAppBoolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool usuarioCadastrado = false;
+            bool pagamentoRecebido = true;
+            var usuarioExpirado = false;
+        }
+    }
+}
