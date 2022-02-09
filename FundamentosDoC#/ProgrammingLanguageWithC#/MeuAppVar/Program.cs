c0a0e25feb811ed13212ce5289e64868db5fd17b
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-09 10:47:13 -0300

    Understanding the use of "var".

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppVar/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MeuAppVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var idade = 25; // Tipo int
+            //idade = "Luis"; // <-- errado
+
+            int idadeInt = 25;
+        }
+    }
+}
