d7705beb168470a37076c5f711adc06db6de424d
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-09 10:39:34 -0300

    Learn how to use "String".

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppString/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MeuAppString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string primeiraLetra = ''; // <-- Errado
+            //string primeiraLetra = "C"; // <-- Correto
+
+            string primeiraLetra = "C";
+            string texto = "Meu texto";
+            var documento = "123456";
+        }
+    }
+}
