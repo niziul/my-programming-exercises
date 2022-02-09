859e69212a77676a175fa804c7337342f42f6959
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-09 11:07:06 -0300

    Understanding the use of Nullable Types

---
+++ FundamentosDoC#/ProgrammingLanguageWithC#/MeuAppNullableTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MeuAppNullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int idade = null; // <-- jeito errado
+            // int? idade = null; // <- jeito correto
+
+            // int? idadeNull = null;
+            // byte? t = null;
+
+            int? idade = 0;
+            Console.WriteLine(idade);
+            idade = null;
+            Console.WriteLine(idade);
+            idade = 25;
+            Console.WriteLine(idade);
+        }
+    }
+}
