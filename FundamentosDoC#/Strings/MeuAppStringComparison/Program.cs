f0349b9d5d4ad77d206e5828b244592f725f780d
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-12 18:32:39 -0300

    Getting a know a little bit about string comparison

---
+++ FundamentosDoC#/Strings/MeuAppStringComparison/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MeuAppStringComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var texto = "Testando";
+            var texto = "Este texto é um teste!";
+
+            // Console.WriteLine(texto.CompareTo("Testando"));
+            // Console.WriteLine(texto.CompareTo("testando"));
+            
+            Console.WriteLine(texto.CompareTo("teste!"));
+
+
+            // Console.WriteLine(texto.Contains("teste")); // input: True
+            // Console.WriteLine(texto.Contains("Teste")); // input: False
+
+            /*
+                O argumento abaixo "StringComparison.OrdinalIgnoreCase" desabilita a sensibilidade do método de identificar 
+                a diferença entre um character em maiúsculo ou minusculo. 
+            */
+            // Console.WriteLine(texto.Contains("Teste", StringComparison.OrdinalIgnoreCase)); // input: True
+
+            // Console.WriteLine(texto.Contains(null)); // Não funciona com argumentos
+
+
+
+
+        }
+    }
+}
