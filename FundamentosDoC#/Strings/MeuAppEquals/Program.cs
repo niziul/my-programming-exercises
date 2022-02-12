5b4e0a3e9e1259ea58defc2f6e445761d5ebf07c
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-12 19:30:14 -0300

    Understanding the operation of equals

---
+++ FundamentosDoC#/Strings/MeuAppEquals/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MeuAppEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Este texto é um teste!";
+            var teste = 33;
+
+            Console.WriteLine(teste.Equals(44));
+
+
+            Console.WriteLine(texto.Equals("Este texto é um teste!")); // true 
+            Console.WriteLine(texto.Equals("este texto é um teste!")); // false
+            Console.WriteLine(texto.Equals("este texto é um teste!", StringComparison.OrdinalIgnoreCase)); // true
+        }
+    }
+}
