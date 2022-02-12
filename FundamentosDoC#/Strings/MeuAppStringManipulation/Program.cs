97eefc9d6f6200180fa9adcbdec767cbf85d559f
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-12 20:14:18 -0300

    Gaining insight into string manipulation

---
+++ FundamentosDoC#/Strings/MeuAppStringManipulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MeuAppStringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = " Este texto é um teste!";
+
+            // Console.WriteLine(texto.Replace("Este", "isto"));
+            // Console.WriteLine(texto.Replace(" ", "-"));
+
+            var divisao = texto.Split(" ");
+
+            Console.WriteLine(divisao[0]);
+            Console.WriteLine(divisao[1]);
+            Console.WriteLine(divisao[2]);
+            Console.WriteLine(divisao[3]);
+
+            var resultado = texto.Substring(0, 5);
+
+            Console.WriteLine(resultado);
+
+
+            Console.WriteLine(texto.Trim());
+        }
+    }
+}
