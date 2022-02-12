b4ee94a56b1cc6022703a32a3fdc3d6fcaf6eb8e
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-12 18:55:53 -0300

    Getting to know a little more about the starts with and ends with methods

---
+++ FundamentosDoC#/Strings/MeuAppStringStartsWith-EndsWith/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MeuAppStringStartsWith_EndsWith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Este texto é um teste!";
+
+            Console.WriteLine(texto.StartsWith("Este")); // True
+            Console.WriteLine(texto.StartsWith("este")); // False
+            Console.WriteLine(texto.StartsWith("texto")); // False
+            
+            Console.WriteLine("----------------"); 
+            
+            Console.WriteLine(texto.EndsWith("teste!")); // true
+            Console.WriteLine(texto.EndsWith("Teste")); // False
+            Console.WriteLine(texto.EndsWith("xpro")); // False
+        
+        }
+    }
+}
