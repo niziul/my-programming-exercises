6012c62d4a92cff78d18731d8ce1a88ad1e226d3
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-12 20:00:50 -0300

    Seeing the usability of index

---
+++ FundamentosDoC#/Strings/MeuAppIndexOf/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MeuAppIndexOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Este texto é um teste!";
+
+            Console.WriteLine(texto.IndexOf("é"));
+            Console.WriteLine(texto.LastIndexOf("s"));
+        }
+    }
+}
