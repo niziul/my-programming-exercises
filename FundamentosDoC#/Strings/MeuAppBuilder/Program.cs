11503dd9c2cf5dfe7c6b8fe6b7c3d5cd392ff2b5
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-12 21:32:38 -0300

    Learning about String builder

---
+++ FundamentosDoC#/Strings/MeuAppBuilder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace MeuAppBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = new StringBuilder();
+
+            texto.Append("Este texto é um teste!\n");
+            texto.Append("Primeira linha\n");
+            texto.Append("Segunda linha\n");
+            texto.Append("Terceira linha\n");
+            texto.Append("Quarta linha\n");
+            texto.Append("Quinta linha");
+
+            texto.ToString();
+            Console.WriteLine(texto);
+            // texto += " aqui";
+        }
+    }
+}
