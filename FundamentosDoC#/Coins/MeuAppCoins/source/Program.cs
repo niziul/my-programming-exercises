6e8cd7a16207e2626b2512747c26f3f6e9f375c7
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-15 20:26:14 -0300

    Type for coins

---
+++ FundamentosDoC#/Coins/MeuAppCoins/source/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+
+            /*
+                Tipo para moedas:
+            */
+
+            // var valor = 10.25;
+            
+            decimal valor = 10.25m;
+            Console.WriteLine(valor);
+        }
+    }
+}
