df5d35424529fb97e65fb353edba8b816d8a66d4
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-15 20:43:16 -0300

    Formatting coins

--- FundamentosDoC#/Coins/MeuAppCoins/source/Program.cs
+++ FundamentosDoC#/Coins/MeuAppCoins/source/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Numbers
 {
@@ -15,8 +16,54 @@ namespace Numbers
 
             // var valor = 10.25;
             
-            decimal valor = 10.25m;
-            Console.WriteLine(valor);
+            // decimal valor = 10.25m;
+            // Console.WriteLine(valor);
+
+
+
+
+
+
+
+
+            /*
+                Formatando moedas:            
+            */
+
+            var valor = 10.25m;
+
+            // var culture = CultureInfo.CurrentCulture;
+
+            // Console.WriteLine(valor.ToString(CultureInfo.CreateSpecificCulture("en-US")));
+
+            // Console.WriteLine(valor.ToString(CultureInfo.CreateSpecificCulture("en-UK")));
+
+            // Console.WriteLine(valor.ToString(CultureInfo.CreateSpecificCulture("pt-PT")));
+
+            // Console.WriteLine(valor.ToString(CultureInfo.CreateSpecificCulture("es-ES")));
+
+            // Console.WriteLine(valor.ToString(CultureInfo.CreateSpecificCulture("pt-BR")));
+
+
+
+            // Console.WriteLine(valor.ToString("G", CultureInfo.CreateSpecificCulture("pt-BR"))); // 10,25
+
+            // Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))); // R$ 10,25
+
+            // Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("es-ES"))); // 10,25 €
+
+            // Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("en-US"))); // $10.25
+
+            // Console.WriteLine(valor.ToString("E04", CultureInfo.CreateSpecificCulture("en-US"))); // 1.0250E+001
+
+            // Console.WriteLine(valor.ToString("N", CultureInfo.CreateSpecificCulture("en-US"))); // 10.250
+
+            Console.WriteLine(valor.ToString("P", CultureInfo.CreateSpecificCulture("en-US"))); // 1,025.000%
+
+
+
+
+
         }
     }
 }
