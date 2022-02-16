241ef11380f3391cce1523091d7fd539c08a93c7
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-16 13:12:39 -0300

    Understanding how try/catch works

--- FundamentosDoC#/Exceptions/MeuAppExceptions/source/Program.cs
+++ FundamentosDoC#/Exceptions/MeuAppExceptions/source/Program.cs
@@ -12,15 +12,40 @@ namespace MeuAppExceptions
                 Tratamento de erros:
             */
 
-            var arr = new int[3];
+            // var arr = new int[3];
             
-            for (int index = 0; index < 10; index++)
-            {
-                // System.IndexOutOfRangeException
-                Console.WriteLine(arr[index]);
+            // for (int index = 0; index < 10; index++)
+            // {
+            //     // System.IndexOutOfRangeException
+            //     Console.WriteLine(arr[index]);
+            // }
+
+
+
+
+
+
+            /*
+                Try/Catch
+            */
+
+            var arr = new int[3];
+
+            try
+            {                
+                for (int index = 0; index < 10; index++)
+                {
+                    Console.WriteLine(arr[index]);
+                }
+    
             }
-
-
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                
+                Console.WriteLine("Ops, algo deu errado!");
+            }
             
         }
     }
