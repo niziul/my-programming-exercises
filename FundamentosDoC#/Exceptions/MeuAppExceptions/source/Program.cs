e72029588eb22f42cb7ef49ad38b5a5d716efb04
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-16 13:03:34 -0300

    Understanding what an exception is

---
+++ FundamentosDoC#/Exceptions/MeuAppExceptions/source/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MeuAppExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            /*
+                Tratamento de erros:
+            */
+
+            var arr = new int[3];
+            
+            for (int index = 0; index < 10; index++)
+            {
+                // System.IndexOutOfRangeException
+                Console.WriteLine(arr[index]);
+            }
+
+
+            
+        }
+    }
+}
