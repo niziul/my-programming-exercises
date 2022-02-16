d827b7573e3ad161cbb6b2f9672dce796c27ba8f
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-16 12:13:20 -0300

    ForEach

--- FundamentosDoC#/Arrays/MeuAppArrays/Program.cs
+++ FundamentosDoC#/Arrays/MeuAppArrays/Program.cs
@@ -60,19 +60,44 @@ namespace MeuAppArrays
                 Percorrendo um array:
             */
 
-            var meuArray = new int[5]{ 1, 2, 3, 4, 5 };
-                meuArray[0] = 12;
-                meuArray[1] = 15;
+            // var meuArray = new int[5]{ 1, 2, 3, 4, 5 };
+            //     meuArray[0] = 12;
+            //     meuArray[1] = 15;
 
-            // Console.WriteLine(meuArray.Length); // 5
+            // // Console.WriteLine(meuArray.Length); // 5
 
-            for (var index = 0; index < meuArray.Length; index++)
-                Console.WriteLine(meuArray[index]);
-
-
+            // for (var index = 0; index < meuArray.Length; index++)
+            //     Console.WriteLine(meuArray[index]);
 
 
 
+
+
+
+
+
+            /*
+                ForEach:
+            */
+
+            var meuArray = new int[5] { 1, 2, 3, 4, 5};
+            meuArray[0] = 12;
+
+            var funcionarios = new Funcionario[5];
+                funcionarios[0] = new Funcionario() { Id = 2579, Nome = "Luis"};
+
+            // Console.WriteLine(meuArray.Length);
+
+            foreach (var item in meuArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var funcionario in funcionarios)
+            {
+                Console.WriteLine(funcionario.Nome);
+                Console.WriteLine(funcionario.Id);
+            }
 
 
 
@@ -87,6 +112,12 @@ namespace MeuAppArrays
         struct test
         {
             public int Id {get; set;}
+        }
+
+        public struct Funcionario
+        {
+            public int Id {get; set;}
+            public string Nome {get; set;}
         }
     }
 }
