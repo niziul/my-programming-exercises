4a601c04efb9a13adf5a56d337da93f2aea02a12
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-16 13:47:35 -0300

    Creating Exceptions

--- FundamentosDoC#/Exceptions/MeuAppExceptions/source/Program.cs
+++ FundamentosDoC#/Exceptions/MeuAppExceptions/source/Program.cs
@@ -92,6 +92,52 @@ namespace MeuAppExceptions
                 Disparando exceções:
             */
 
+            // var arr = new int[3];
+
+            // try
+            // {                
+            //     // for (int index = 0; index < 10; index++)
+            //     // {
+            //     //     // IndexOutOfRangeException: index fora de alcance
+            //     //     Console.WriteLine(arr[index]);
+            //     // }
+
+            //     Cadastrar("");
+            // }
+            // catch (IndexOutOfRangeException ex)
+            // {
+            //     Console.WriteLine(ex.InnerException);
+            //     Console.WriteLine(ex.Message);
+            //     Console.WriteLine("Não encontrei o índice na lista");
+            // }
+            // catch (ArgumentNullException ex)
+            // {
+            //     Console.WriteLine(ex.InnerException);
+            //     Console.WriteLine(ex.Message);
+            //     Console.WriteLine("Falha ao cadastrar texto");
+            // }
+            // catch (Exception ex)
+            // {
+            //     Console.WriteLine(ex.InnerException);
+            //     Console.WriteLine(ex.Message);
+
+            //     Console.WriteLine("Ops, algo deu errado!");
+            // }
+            
+
+
+
+
+
+
+
+
+
+            /*
+                Custom Exceptions:
+            */
+
+            
             var arr = new int[3];
 
             try
@@ -103,6 +149,13 @@ namespace MeuAppExceptions
                 // }
 
                 Cadastrar("");
+            }
+            catch (MinhaException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.QuandoAconteceu);
+                Console.WriteLine("Exceção customizada");
             }
             catch (IndexOutOfRangeException ex)
             {
@@ -123,18 +176,23 @@ namespace MeuAppExceptions
 
                 Console.WriteLine("Ops, algo deu errado!");
             }
-            
         }
         
         private static void Cadastrar(string texto)
         {
             if (string.IsNullOrEmpty(texto))
                 // throw new Exception("O texto não pode ser nulo ou vazio");
-                throw new ArgumentNullException("O texto não pode ser nulo ou vazio");
+                // throw new ArgumentNullException("O texto não pode ser nulo ou vazio");
+                throw new MinhaException(DateTime.Now);
         }
-        static void Salvar(string texto)
+        
+        public class MinhaException : Exception
         {
-            
+            public MinhaException(DateTime date)
+            {
+                QuandoAconteceu = date;
+            }
+            public DateTime QuandoAconteceu {get; set;}
         }
     }
 }
