81dd036bca3bcdf731589710eabf375d923ee8f5
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-24 15:22:51 -0300

    Encapsulation

---
+++ Object-orientation/Fundamentals/Encapsulation/Payments/Program.cs
@@ -0,0 +1,20 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AnsiConsole.Write(new Markup(""));
+    }
+
+    class Payments
+    {
+        // Propriedades:
+        DateTime DueDate;
+
+        // Métodos:
+        void Pay(){}
+    }
+}
