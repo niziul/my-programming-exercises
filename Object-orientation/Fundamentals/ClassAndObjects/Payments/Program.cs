0abd8a7c14a522f95496d8d15e26f2cbabdb459f
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-24 15:11:40 -0300

    Knowing the differences from classes to structures

---
+++ Object-orientation/Fundamentals/ClassAndObjects/Payments/Program.cs
@@ -0,0 +1,35 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+            Tipo de referência
+            -> Endereço dos dados
+        */ 
+
+        var customer = new Customer();
+        // customer.Name = "Test";
+        customer.name = "Pedro";
+
+        
+        AnsiConsole.Write(new Markup($"[underline]Nome do cliente[/]:[bold blue]{customer.name}[/]"));
+    }
+
+    /*
+        Diferença de classes para struct:
+    */
+
+    // struct Customer
+    // {
+    //     public string Name;
+    // }
+
+    class Customer 
+    {
+        public string ?name;
+    }
+}   
