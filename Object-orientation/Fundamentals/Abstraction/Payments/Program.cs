ff8a1879163a937ffe2b9e9ca68da1d87760e51a
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-25 15:07:16 -0300

    Understanding a little more about abstraction

---
+++ Object-orientation/Fundamentals/Abstraction/Payments/Program.cs
@@ -0,0 +1,26 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+    class Payments
+    {
+        DateTime DueDate;
+
+        void Pay()
+        {
+            ConsultCardBalance();
+        }
+
+        private void ConsultCardBalance()
+        {
+
+        }
+    }
+}
