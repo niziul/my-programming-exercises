1c7af33b7cc95f0b34920f1d972ec6225b46aa28
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-02-28 20:10:18 -0300

    Getting to know a little about complex types

---
+++ Object-orientation/Fundamentals/Complex-Types/Payments/Program.cs
@@ -0,0 +1,45 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var TheTitle = new Rule();
+            TheTitle.RuleTitle("[red]Payment Information[/]");
+            TheTitle.Alignment = Justify.Left;
+
+        var Payments = new Payments();
+            
+
+        AnsiConsole.Clear();
+        AnsiConsole.Write(TheTitle);
+        AnsiConsole.Write(new Markup($"Due Date: [strikethrough #00ff00]empty[/];"));
+        AnsiConsole.WriteLine();
+        AnsiConsole.Write(new Markup($"Number Billet: [strikethrough #00ff00]empty[/];"));
+    }
+
+    /*
+        Modificadores de acesso:
+
+            - Private, protected, internal e public
+    */
+
+    public class Payments
+    {
+        // DateTime DueDate;
+        // private DateTime DueDate;
+        DateTime DueDate;
+        Address ?BillingAddress;
+        // void Pay(){}
+        // private void Pay(){}
+        void Pay(){}
+  
+    }
+
+    public class Address
+    {
+        string ?ZipCode;
+    }
+}
