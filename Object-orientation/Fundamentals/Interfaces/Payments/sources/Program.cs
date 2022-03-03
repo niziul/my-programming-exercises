81fa98f019e0988391e52bb23523431ce7dcdfcb
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-03 15:52:07 -0300

    Learning how to create interfaces

---
+++ Object-orientation/Fundamentals/Interfaces/Payments/sources/Program.cs
@@ -0,0 +1,38 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program 
+{ 
+    public static void Main(string[] args) 
+    {            
+       
+    }
+
+    public class Payment : IPayment
+    {
+        public DateTime DueDate { get; set; }
+
+        public void Pay(double value)
+        {
+            
+        }
+    }
+
+    public class CreditCard : IPayment
+    {
+        public DateTime DueDate { get; set; }
+
+        public void Pay(double value)
+        {
+            
+        }
+    }
+
+    public interface IPayment 
+    {
+        DateTime DueDate { get; set; }
+
+        void Pay(double value);
+    }
+}
