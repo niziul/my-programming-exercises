2ff89ac3de6c0e7a55f056c2c88d621921ccf95f
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-03 00:32:41 -0300

    Learning about static classes

---
+++ Object-orientation/Fundamentals/Static-Classes/Payments/Program.cs
@@ -0,0 +1,23 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program
+{
+    public static void Main(string[] args) 
+    { 
+        Payments.DueDate = DateTime.Now;
+
+        AnsiConsole.Write(new Markup($"[blue]{Payments.DueDate}[/]")); 
+    }
+    
+    public static class Payments 
+    { 
+        public static DateTime DueDate { get; set; }
+    }
+
+    public static class Settings
+    {
+        public static string? API_URL { get; set; }
+    }
+}
