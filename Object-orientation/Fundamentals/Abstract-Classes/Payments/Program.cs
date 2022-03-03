45caff2d215a7a2234b7392885fc5cd748bf749f
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-03 19:06:08 -0300

    familiarizing with abstract classes

---
+++ Object-orientation/Fundamentals/Abstract-Classes/Payments/Program.cs
@@ -0,0 +1,36 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program 
+{ 
+    public static void Main(string[] args) 
+    {            
+       var payment = new CreditCard();
+    }
+
+    public abstract class Payment : IPayment
+    {
+        public DateTime DueDate { get; set; }
+
+        public virtual void Pay(double value)
+        {
+            // Executar
+        }
+    }
+
+    public class CreditCard : Payment { public override void Pay(double value) { base.Pay(value); } }
+
+    public class ApplePay : Payment { public override void Pay(double value) { base.Pay(value); } }
+
+    public class Billet : Payment { public override void Pay(double value) { base.Pay(value); } }
+
+
+
+    public interface IPayment 
+    {
+        DateTime DueDate { get; set; }
+
+        void Pay(double value);
+    }
+}
