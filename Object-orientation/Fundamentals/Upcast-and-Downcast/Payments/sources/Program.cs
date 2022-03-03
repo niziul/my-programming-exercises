e40130e49e1b58fe9fa86406f2a587ffde7edb71
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-03 19:35:21 -0300

    upcast and downcast

---
+++ Object-orientation/Fundamentals/Upcast-and-Downcast/Payments/sources/Program.cs
@@ -0,0 +1,25 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program 
+{ 
+    public static void Main(string[] args) 
+    {            
+        // Upcast:
+        var person = new Person();
+            person = new IndividualPerson();
+        // ::
+        
+        var legalEntity = new LegalEntity();     
+        var individualPerson = new IndividualPerson();
+
+        // Downcast:
+        individualPerson = (IndividualPerson)person;
+        // ::
+    }
+
+    public class Person { public string? Name { get; set; } }
+    public class LegalEntity : Person { public string? CNPJ { get; set; } }
+    public class IndividualPerson : Person { public string? CPF { get; set; } }
+}
