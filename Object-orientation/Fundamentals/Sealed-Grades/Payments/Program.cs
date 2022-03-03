bc3e1dbe8083464e22c220bb166a8479a676227b
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-03 00:38:40 -0300

    Getting to know a little bit about sealed classes

---
+++ Object-orientation/Fundamentals/Sealed-Grades/Payments/Program.cs
@@ -0,0 +1,16 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program
+{
+    public static void Main(string[] args) 
+    { 
+        AnsiConsole.Write(new Markup($"[blue]{Payments.DueDate}[/]")); 
+    }
+    
+    public sealed class Payments 
+    { 
+        public static DateTime DueDate { get; set; }
+    }    
+}
