7c220ea6a29c7b60fc10378a6ff9ef04f715c7d3
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-02 22:02:49 -0300

    Learning the usability of "using" and "dispose"

---
+++ Object-orientation/Fundamentals/Using-and-Dispose/Payments/Program.cs
@@ -0,0 +1,34 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // var Payments = new Payments();
+            // Payments.Dispose();
+        
+        AnsiConsole.Clear();
+        
+        using(var payments = new Payments()) 
+        { 
+            AnsiConsole.Status().Spinner(Spinner.Known.Dots).Start("[green]Processing the payment[/]", ctx => { Thread.Sleep(3000); }); 
+            AnsiConsole.WriteLine();
+            AnsiConsole.Write(new Markup("[dim green]Processing payment[/] [green]:check_mark:[/]")); 
+            AnsiConsole.WriteLine(); Thread.Sleep(2000); 
+            AnsiConsole.Clear(); 
+        }
+        
+    }
+
+    public class Payments : IDisposable
+    {
+        public Payments() { AnsiConsole.Status().Spinner(Spinner.Known.Dots).Start("[blue]Initiating payment[/]", ctx => { Thread.Sleep(3000); }); }
+
+        /*
+            Garbage Collector:
+        */
+        public void Dispose() { AnsiConsole.Status().Spinner(Spinner.Known.Dots).Start("[red]Finalizing the payment[/]", ctx => { Thread.Sleep(3000); }); }
+    }
+} 
