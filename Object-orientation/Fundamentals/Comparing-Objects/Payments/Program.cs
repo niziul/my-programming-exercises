4f636f595ad05409312a21d8e3f68a00117686ee
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-03 20:50:22 -0300

    Learning how to compare objects

---
+++ Object-orientation/Fundamentals/Comparing-Objects/Payments/Program.cs
@@ -0,0 +1,32 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program 
+{ 
+    public static void Main(string[] args) 
+    {            
+        var personA = new Person(1, "Hunter");
+        var personB = new Person(1, "Hunter");
+
+        // AnsiConsole.Write(personA == personB);
+        // AnsiConsole.Write(personA.Id == personB.Id);
+        AnsiConsole.Write(personA.Equals(personB));
+    }
+
+    public class Person : IEquatable<Person> 
+    { 
+        public int Id { get; set; } 
+        public string Name { get; set; } 
+        
+        public Person(int id, string name) 
+        {
+            Id = id; 
+            Name = name; 
+        }
+
+
+        public bool Equals(Person person) => Id == person.Id; 
+    }
+
+}
