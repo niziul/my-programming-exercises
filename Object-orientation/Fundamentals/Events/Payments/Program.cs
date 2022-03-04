bbc4d84c89189cc59d6c2a3aa3176eb352c73f84
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-04 16:47:31 -0300

    Events

---
+++ Object-orientation/Fundamentals/Events/Payments/Program.cs
@@ -0,0 +1,52 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program 
+{ 
+    static void Main(string[] args) 
+    { 
+        var room = new Room(3); 
+            room.RoomSoldOutEvent += OnRoomSoldOut;
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+    } 
+    static void OnRoomSoldOut(object sender, EventArgs e)
+    {
+        AnsiConsole.WriteLine("Sala lotada.");
+    }
+}
+
+public class Room 
+{ 
+    public int Seats { get; set; }
+
+
+    private int seatsInUse = 0;
+    
+    public Room(int seats) 
+    { 
+        Seats = seats; 
+        seatsInUse = 0; 
+    }
+    
+    public void ReserveSeat() 
+    { 
+        seatsInUse++; 
+        if(seatsInUse > Seats) 
+            OnRoomSoldOut(EventArgs.Empty);
+        else
+            AnsiConsole.WriteLine("Assento reservado!"); 
+    }
+
+    public event EventHandler RoomSoldOutEvent;
+
+    protected virtual void OnRoomSoldOut(EventArgs e) 
+    {
+        EventHandler handler = RoomSoldOutEvent;
+        handler?.Invoke(this, e);
+    }
+}
