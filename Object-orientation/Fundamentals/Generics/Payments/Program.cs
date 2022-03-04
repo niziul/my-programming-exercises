2d527f29277c6b6eea0dceb104b0e423ea000471
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-04 19:48:35 -0300

    Conclusion of the first part regarding generic types

---
+++ Object-orientation/Fundamentals/Generics/Payments/Program.cs
@@ -0,0 +1,29 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var person = new Person();
+        var payments = new Payments();
+        var subscription = new Subscription();
+
+        var context = new DataContext<Person, Payments, Subscription>();
+            context.Save(person);
+            context.Save(payments);
+            context.Save(subscription);
+    }
+}
+
+public class DataContext<P, PA, S>
+{
+    public void Save(P entity) { }
+    public void Save(PA entity) { }
+    public void Save(S entity) { }
+}
+
+class Person { }
+class Payments { }
+class Subscription { }
