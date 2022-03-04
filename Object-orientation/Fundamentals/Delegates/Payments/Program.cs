99dcce055246e3e6290a4bc7716c70d17e4e1cf9
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-04 16:20:44 -0300

    First contact with anonymous methods

---
+++ Object-orientation/Fundamentals/Delegates/Payments/Program.cs
@@ -0,0 +1,22 @@
+﻿using Spectre.Console;
+
+namespace Payments;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        // Delegates => Anonymous Methods:
+        var pay = new Payments.Pay(RealizesPayment);
+            pay(25);
+        // ::
+    }
+
+    public class Payments { public delegate void Pay(double value); }
+ 
+    public static void RealizesPayment(double value) 
+    { 
+        AnsiConsole.Write(new Markup($"[blue]I paid the amount of[/] [dim red]${value}[/]")); 
+    }
+}
