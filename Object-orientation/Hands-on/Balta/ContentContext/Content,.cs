176401b6a8787fc3b74e504bcf2e17be058254cd
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-05 15:44:19 -0300

    I created a constructor to the Content class to automatically generate a new globally unique identifier

--- Object-orientation/Hands-on/Balta/ContentContext/Content,.cs
+++ Object-orientation/Hands-on/Balta/ContentContext/Content,.cs
@@ -2,6 +2,8 @@ namespace Balta.ContentContext;
 
 public class Content
 {
+    public Content() { Id = Guid.NewGuid(); }
+
     public Guid Id { get; set; }
     public string Title { get; set; }
     public string Url { get; set; }
