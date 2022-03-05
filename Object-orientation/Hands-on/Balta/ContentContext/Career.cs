0e5b5137204bf56aa4384e64037250590b11dfa4
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-05 18:32:20 -0300

    Modeling the Career

--- Object-orientation/Hands-on/Balta/ContentContext/Career.cs
+++ Object-orientation/Hands-on/Balta/ContentContext/Career.cs
@@ -1,3 +1,15 @@
 namespace Balta.ContentContext;
 
-public class Career : Content { public int Courses { get; set; } }
+public class Career : Content 
+{ 
+    public Career()
+    {
+        Items = new List<CareerItem>();
+    }
+
+    public IList<CareerItem> Items { get; set; } 
+    // Expression body
+    public int TotalCourses => Items.Count;
+
+}
+
---
+++ Object-orientation/Hands-on/Balta/ContentContext/CareerItem.cs
@@ -0,0 +1,9 @@
+namespace Balta.ContentContext;
+
+public class CareerItem
+{
+    public int Ordem { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Course Course { get; set; }
+}
