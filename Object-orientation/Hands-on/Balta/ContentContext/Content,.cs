62e7bac75ce10eb1f67ff6b0ab991505544216e8
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-05 15:39:51 -0300

    Changed the Id type from integer to globally unique identifier

--- Object-orientation/Hands-on/Balta/ContentContext/Content,.cs
+++ Object-orientation/Hands-on/Balta/ContentContext/Content,.cs
@@ -2,7 +2,7 @@ namespace Balta.ContentContext;
 
 public class Content
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string Title { get; set; }
     public string Url { get; set; }
 }
