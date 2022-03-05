611bf48c8103ae64b5dbb373125d6acfb652cf15
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-05 16:05:55 -0300

    Working with lists

--- Object-orientation/Hands-on/Balta/ContentContext/Course.cs
+++ Object-orientation/Hands-on/Balta/ContentContext/Course.cs
@@ -1,3 +1,25 @@
 namespace Balta.ContentContext;
 
-public class Course : Content { public string Tag { get; set; } }
+public class Course : Content 
+{ 
+    public string Tag { get; set; } 
+    public IList<Module> Modules { get; set; } 
+}
+
+public class Module 
+{ 
+    public Module()
+    {
+        Lectures = new List<Lecture>();
+    }
+
+    public int Order { get; set; }
+    public string Title { get; set; }
+    public IList<Lecture> Lectures { get; set; }
+}
+
+public class Lecture
+{
+    public int Ordem { get; set; }
+    public string Title { get; set; }
+}
