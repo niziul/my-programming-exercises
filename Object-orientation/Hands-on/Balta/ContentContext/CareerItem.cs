66e77a6b069340b81720fd4c98afa618d3d6d19f
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-07 15:50:29 -0300

    Business Rules

--- Object-orientation/Hands-on/Balta/ContentContext/CareerItem.cs
+++ Object-orientation/Hands-on/Balta/ContentContext/CareerItem.cs
@@ -4,6 +4,9 @@ public class CareerItem
 {
     public CareerItem(int order, string title, string description, Course course)
     {
+        if(course == null)
+            throw new SystemException("O curso não pode ser nulo");
+
         Order = order;
         Title = title;
         Description = description;
--- Object-orientation/Hands-on/Balta/Program.cs
+++ Object-orientation/Hands-on/Balta/Program.cs
@@ -17,18 +17,21 @@ class Program
         var careers = new List<Career>();  
 
         var careerDotnet = new Career("Especialista .NET", "especialista-dotnet");
-        var careerItem = new CareerItem(1, "Comece por aqui", "", null);
+        var careerItem = new CareerItem(1, "Comece por aqui", "", courseOOP);
 
         careerDotnet.Items.Add(careerItem);
         careers.Add(careerDotnet);
 
         foreach(var career in careers)
         {
-            AnsiConsole.Write(career.Title);
-            AnsiConsole.WriteLine();
+            AnsiConsole.Write(new Rule($"[red]{career.Title}[/]"));
             foreach(var item in career.Items)
             {
-                AnsiConsole.Write($"{item.Order} - {item.Title}");
+                AnsiConsole.Write(new Markup($"{item.Order} - [underline blue]{item.Title}[/]"));
+                AnsiConsole.WriteLine();
+                AnsiConsole.Write(new Markup($"[green]Title[/]: {item.Course.Title}"));
+                AnsiConsole.WriteLine();
+                AnsiConsole.Write(new Markup($"[green]Level[/]: {item.Course.Level}"));
             }
         }
     }
