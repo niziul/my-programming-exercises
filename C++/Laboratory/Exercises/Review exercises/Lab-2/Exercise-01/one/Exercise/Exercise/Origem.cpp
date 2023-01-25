f676ae5071a832f6e0a7859b306ce0d95616cf7f
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-01-24 20:14:23 -0800

    Delete C++ directory

--- C++/Laboratory/Exercises/Review exercises/Lab-2/Exercise-01/one/Exercise/Exercise/Origem.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	std::cout << "Welcome" << std::endl << "to the" << std::endl << "C++ world!";
-	std::cout << std::endl;
-
-	return 0;
-}
--- C++/Laboratory/Exercises/Review exercises/Lab-2/Exercise-01/two/Exercise/Exercise/Origem.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	std::cout << "Welcome" << "\n" << "to the" << "\n" << "C++ world!";
-	std::cout << "\n";
-
-	return 0;
-}
