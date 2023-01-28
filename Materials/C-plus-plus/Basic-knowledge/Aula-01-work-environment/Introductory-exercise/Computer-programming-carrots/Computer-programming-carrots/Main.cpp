22b0e5bf22ddedd986cb49bb5a9145f48a854511
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-01-28 18:38:12 -0300

    [C++] - Basic Knowledge - Commit - 4

---
+++ Materials/C-plus-plus/Basic-knowledge/Aula-01-work-environment/Introductory-exercise/Computer-programming-carrots/Computer-programming-carrots/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int total_amount_of_carrots = 25;
+
+	cout << "I have " << total_amount_of_carrots << " carrots.";
+	cout << endl;
+
+	total_amount_of_carrots -= 1;
+
+	cout << "Now i have " << total_amount_of_carrots << " carrots.";
+	cout << endl;
+
+	return 0;
+}
