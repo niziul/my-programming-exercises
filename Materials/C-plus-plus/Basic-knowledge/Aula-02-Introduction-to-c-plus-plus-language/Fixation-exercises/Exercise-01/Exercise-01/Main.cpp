c1eb66f0deb748bcc9278c3f4158f38d6d90acff
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-01-30 18:47:55 -0300

    [C++] - Basic Knowledge - Commit - 11
    Conclusion of the first fixation exercise!

---
+++ Materials/C-plus-plus/Basic-knowledge/Aula-02-Introduction-to-c-plus-plus-language/Fixation-exercises/Exercise-01/Exercise-01/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Av. Abramo Randon";
+	cout << endl;
+
+	cout << "770 - Interlagos";
+	cout << endl;
+
+	cout << "Caxias do Sul";
+	cout << endl;
+
+	return 0;
+}
