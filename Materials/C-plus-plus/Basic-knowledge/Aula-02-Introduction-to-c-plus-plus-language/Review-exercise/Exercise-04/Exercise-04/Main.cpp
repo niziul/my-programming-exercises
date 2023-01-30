64c0071ee329ca417f93f20882684e93ba5d70d4
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-01-30 17:29:39 -0300

    [C++] - Basic Knowledge - Commit - 9
    Conclusion of the fourth review exercise!

---
+++ Materials/C-plus-plus/Basic-knowledge/Aula-02-Introduction-to-c-plus-plus-language/Review-exercise/Exercise-04/Exercise-04/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(int argc, char ** argv)
+{
+	cout << argv[1] << " " << argv[2] << " program!" << endl;
+
+	return 0;
+}
