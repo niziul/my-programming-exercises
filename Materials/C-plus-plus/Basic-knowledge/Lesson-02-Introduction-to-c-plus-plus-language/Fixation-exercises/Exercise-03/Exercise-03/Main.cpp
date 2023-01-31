5e74b721640ec6cf7db65b02d1943ab773057614
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-01-31 15:19:54 -0300

    [C++] - Basic Knowledge - Commit - 14
    Lesson - 02 - Introduction to c plus plus language
    
    - Conclusion of the third fixation exercise!
       - Creating some figures with graphical characters;

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-02-Introduction-to-c-plus-plus-language/Fixation-exercises/Exercise-03/Exercise-03/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+	system("chcp 850 > nul");
+
+	cout << "\n\n";
+
+	cout << "\n\t\xDC\xDC\xDB \xDB\xDB\xDB\xDB\xDB\xDB";
+
+	cout << "\n\t\xDFO\xDF\xDF\xDF\xDF\xDFOO\xDF";
+
+	cout << "\n\n";
+
+	cout << "\n\t\xC9\xCD\xCD\xCD\xCD\xCD\xBB";
+
+	cout << "\n\t\xC8\xCD\xCD\xCD\xCD\xCD\xBC";
+
+	cout << "\n\n";
+
+	return 0;
+}
