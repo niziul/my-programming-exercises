f3fa080411fe609529793cd30780939fe5ee5874
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-01-31 13:11:29 -0300

    [C++] - Basic Knowledge - Commit - 12
    Lesson - 02 - Introduction to c plus plus language
    
    - Conclusion of the second fixation exercise!

---
+++ Materials/C-plus-plus/Basic-knowledge/Aula-02-Introduction-to-c-plus-plus-language/Fixation-exercises/Exercise-02/Exercise-02/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Iron" << " " << "Man";
+	
+	cout << endl;
+
+	cout << "Robert";
+	cout << endl;
+	cout << "Downey Jr";
+
+	cout << endl;
+
+	cout << "Iron" << " " << "Man";
+	cout << " - ";
+	cout << "Robert" << " " << "Downey Jr";
+
+	cout << endl;
+
+	return 0;
+}
