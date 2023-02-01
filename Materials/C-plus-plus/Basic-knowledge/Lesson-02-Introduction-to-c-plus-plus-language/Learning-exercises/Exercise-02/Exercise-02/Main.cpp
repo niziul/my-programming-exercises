0c907fe5cdb10b7d507512bf13454b158558b881
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-02-01 19:55:36 -0300

    [C++] - Basic Knowledge - Commit - 17
    Lesson - 02 - Introduction to c plus plus language
    
    - Conclusion of the second learning exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-02-Introduction-to-c-plus-plus-language/Learning-exercises/Exercise-02/Exercise-02/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << endl;
+
+	cout << "\xC9\xCD\xCD\xCD\xCD\xCD\xBB";
+
+	cout << endl;
+
+	cout << "\xC8\xCD\xCD\xCD\xCD\xCD\xBC";
+
+	cout << endl;
+
+	return 0;
+}
