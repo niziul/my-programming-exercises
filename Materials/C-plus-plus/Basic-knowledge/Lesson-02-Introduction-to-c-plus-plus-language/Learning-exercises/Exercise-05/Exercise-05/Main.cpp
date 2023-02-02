aacd2c93811d57f246da4e3d0e2721b805aa91f8
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-02-02 17:47:53 -0300

    [C++] - Basic Knowledge - Commit - 20
    Lesson - 02 - Introduction to c plus plus language
    
    - Conclusion of the fifth learning exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-02-Introduction-to-c-plus-plus-language/Learning-exercises/Exercise-05/Exercise-05/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::system;
+
+int main(int argc, char** argv)
+{
+	system(argv[1]); // run the calculator
+
+	//system(argv[2]); // run the notepad
+
+	//system(argv[3]); // run the mspaint
+
+	return 0;
+}
