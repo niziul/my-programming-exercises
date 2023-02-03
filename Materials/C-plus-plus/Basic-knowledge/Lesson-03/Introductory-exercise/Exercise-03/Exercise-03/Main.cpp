45f0e227b151cb8716ef28f3bd2abfc42f4d038c
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-02-03 19:49:43 -0300

    [C++] - Basic Knowledge - Commit - 25
    Lesson - 03 - Variables, Constants and Assignment
    
    - Conclusion of the third introductory exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-03/Introductory-exercise/Exercise-03/Exercise-03/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "The letter" << " " << 'M' << " ";
+	cout << "has" << " " << 3 << " " << "legs." << endl;
+
+	char char_letter = 'M';
+
+	int int_number = 0x1E;
+
+	cout << "Ten letters" << " " << char_letter << " ";
+	cout << "has" << " " << int_number << " " << "legs." << endl;
+
+	return 0;
+}
