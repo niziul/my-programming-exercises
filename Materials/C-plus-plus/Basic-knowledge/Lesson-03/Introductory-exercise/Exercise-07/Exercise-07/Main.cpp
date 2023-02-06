49be19009226830962a50733b9c0eb4646bea492
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-02-05 21:19:55 -0300

    [C++] - Basic Knowledge - Commit - 29
    Lesson - 03 - Variables, Constants and Assignment
    
    - Conclusion of the seventh introductory exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-03/Introductory-exercise/Exercise-07/Exercise-07/Main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int carrots;
+	int lettuces;
+	int pineapple;
+
+	cout << "carrots: ";
+	cin >> carrots;
+
+	cout << "lettuces: ";
+	cin >> lettuces;
+
+	cout << "pineapple: ";
+	cin >> pineapple;
+
+	cout << endl;
+
+	cout << "Amount of corrots: " << carrots << endl;
+	cout << "Amount of lettuces: " << lettuces << endl;
+	cout << "Amount of pineapple: " << pineapple << endl;
+
+	cout << endl;
+
+	return 0;
+}
