61b8a0c6eadfe894052b460731d2d98e2f384265
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-02-08 20:06:59 -0300

    [C++] - Basic Knowledge - Commit - 33
    Lesson - 03 - Variables, Constants and Assignment
    
    - Conclusion of the second fixation exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-03/Fixation-exercises/Exercise-02/Exercise-02/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int measure = 10;
+
+	cout << "measure = " << measure << endl;
+	
+	cout << "2x measure = " << measure * 2 << endl;
+
+	cout << "squared measure = " << measure * measure << endl;
+
+	return 0;
+}
