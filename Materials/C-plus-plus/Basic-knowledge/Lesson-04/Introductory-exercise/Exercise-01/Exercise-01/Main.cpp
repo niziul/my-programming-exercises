ce4f6cc4d439cdde2bc73ea3734c50fd8286b720
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-02-12 20:07:37 -0300

    [C++] - Basic Knowledge - Commit - 42
    Lesson - 04 - Modularity and Functions
    
    - Conclusion of the first introductory exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-04/Introductory-exercise/Exercise-01/Exercise-01/Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	double area;
+	double side;
+
+	cout << "What is the area of your house in square meters?" << endl;
+	cout << "- area ( ex: 6.25 ) : ";
+	cin >> area;
+
+	cout << endl;
+	cout << endl;
+
+	side = sqrt(area);
+
+	cout << "[ This is equivalent to a square with " << side << " meters on a side. ]" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
