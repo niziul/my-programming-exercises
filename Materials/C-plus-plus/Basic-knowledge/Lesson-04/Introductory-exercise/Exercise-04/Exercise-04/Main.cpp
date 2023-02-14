d53ede6b1721de58458d5091a51f423069f271ce
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-02-14 18:34:02 -0300

    [C++] - Basic Knowledge - Commit - 45
    Lesson - 04 - Modularity and Functions
    
    - Conclusion of the fourth introductory exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-04/Introductory-exercise/Exercise-04/Exercise-04/Main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void another_function(void);
+void auxiliary_function(void);
+
+int main()
+{
+	cout << "A c++ program "
+		 << "always starts with the "
+		 << "main function.";
+
+	cout << endl;
+	cout << endl;
+
+	another_function();
+
+	auxiliary_function();
+
+	cout << "more than once.";
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
+
+void another_function(void)
+{
+	cout << "Functions can also "
+		 << "be invoked from "
+		 << "the maain function.";
+
+	cout << endl;
+
+	auxiliary_function();
+
+	cout << "from other "
+		 << "functions.";
+
+	cout << endl;
+}
+
+void auxiliary_function(void)
+{
+	cout << "Functions can also "
+		 << "be invoked ";
+}
