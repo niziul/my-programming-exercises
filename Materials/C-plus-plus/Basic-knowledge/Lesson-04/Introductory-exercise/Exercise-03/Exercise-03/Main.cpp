d728d7e1272e5b5272fa128bdc3e6f27cb29529b
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-02-14 18:15:05 -0300

    [C++] - Basic Knowledge - Commit - 44
    Lesson - 04 - Modularity and Functions
    
    - Conclusion of the third introductory exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-04/Introductory-exercise/Exercise-03/Exercise-03/Main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void happy_function(void);
+void new_year_function(void);
+void christmas_function(void);
+
+int main()
+{
+	cout << "I wish everyone a ";
+
+	happy_function();
+
+	new_year_function();
+
+	cout << "and a ";
+
+	happy_function();
+
+	christmas_function();
+
+	cout << endl;
+
+	return 0;
+}
+
+void happy_function(void)
+{
+	cout << "Happy ";
+}
+
+void new_year_function(void)
+{
+	cout << "New Year ";
+}
+
+void christmas_function(void)
+{
+	cout << "Christmas ";
+}
