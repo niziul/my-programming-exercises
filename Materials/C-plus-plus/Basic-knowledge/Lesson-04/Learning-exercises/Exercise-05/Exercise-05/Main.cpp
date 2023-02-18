3d4c23f7dbe56b4f1fd596d809d9f42ed419cdef
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-02-18 18:49:29 -0300

    [C++] - Basic Knowledge - Commit - 55
    Lesson - 04 - Modularity and Functions
    
    - Conclusion of the fifth learning exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-04/Learning-exercises/Exercise-05/Exercise-05/Main.cpp
@@ -0,0 +1,52 @@
+/*
+	[ description of the fifth learning exercise ] :
+		- Write a program that asks the user to enter an integer
+		  ( positive or negative ). Create and use a function called absolute() to find
+		  the absolute value of a number. The absolute() function should take an integer value
+		  as parameter and return its absolute value.
+
+	[ output ] :
+		```
+			What is the number ( ex: -10 ) of which you want to know the absolute value?
+			: -10
+
+
+			[ The absolute value of -10 is 10 ]
+		```
+*/
+
+#include <iostream>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int absolute_function(int integer_number);
+
+int main()
+{
+	int integer_number_value;
+	int absolute_number_value;
+
+	cout << "What is the number ( ex: -10 ) of which you want to know the absolute value?" << endl;
+	cout << ": ";
+	cin >> integer_number_value;
+
+	cout << endl;
+	cout << endl;
+
+	absolute_number_value = absolute_function(integer_number_value);
+
+	cout << "[ The absolute value of " << integer_number_value << " is " << absolute_number_value << " ]" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
+
+int absolute_function(int integer_number)
+{
+	return sqrt(pow(integer_number, 2.0));
+}
