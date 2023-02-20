c760a6b3f3ed34024ea0c96504ebbf096092efd2
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-02-19 23:12:54 -0300

    [C++] - Basic Knowledge - Commit - 60
    Lesson - 05 - Libraries, Function Parameters and Arguments
    
    - Conclusion of the third introductory exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-05/Introductory-exercise/Exercise-03/Exercise-03/Main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int biggest_function(int first_value, int second_value);
+
+int main()
+{
+	int first_number;
+	int second_number;
+
+	cout << "Enter two numbers ( ex: 6 7 )" << endl;
+	cout << ": ";
+	cin >> first_number >> second_number;
+
+	cout << endl;
+	cout << endl;
+
+	cout << "[ The biggest of them = " << biggest_function(first_number, second_number) << " ]" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
+
+int biggest_function(int first_value, int second_value)
+{
+	if (first_value > second_value)
+	{
+		return first_value;
+	}
+	else
+	{
+		return second_value;
+	}
+}
