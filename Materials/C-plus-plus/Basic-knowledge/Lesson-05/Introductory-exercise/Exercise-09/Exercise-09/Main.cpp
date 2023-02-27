d4acb7a64a5f3f76eeb2138b4b159ef194fcecca
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-02-27 19:50:05 -0300

    [C++] - Basic Knowledge - Commit - 66
    Lesson - 05 - Libraries, Function Parameters and Arguments
    
    - Conclusion of the ninth introductory exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-05/Introductory-exercise/Exercise-09/Exercise-09/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int x;
+
+int main()
+{
+	cout << endl;
+	cout << endl;
+
+	cout << x << endl;
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
