b9b4f93caf65c60bc6199fdf17da0b7aae64380f
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-02-28 21:07:56 -0300

    [C++] - Basic Knowledge - Commit - 69
    Lesson - 05 - Libraries, Function Parameters and Arguments
    
    - Second Review Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-05/Review-exercise/Exercise-02/Exercise-02/Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int num;
+
+int f();
+int g();
+
+int main()
+{
+	using std::cout;
+	using std::endl;
+
+	num = 1;
+	
+	cout << f() + g() + num << endl;
+
+	system("pause");
+}
+
+int f()
+{
+	return num = num + 3;
+}
+
+int g()
+{
+	return num = 2;
+}
