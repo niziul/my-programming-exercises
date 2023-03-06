00fde46d88eedd6e834f53f079822550d2a4aa0c
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-03-06 20:53:34 -0300

    [C++] - Basic Knowledge - Commit - 84
    Lesson - 06 - Integer Types, Variables, Constants, Overflow and Underflow
    
    - First Review Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-06/Review-exercise/Exercise-01/Exercise-01/Main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+#define default    "\033[m"
+
+#define green      "\033[32m"
+
+#define red        "\033[4;31m"
+
+#define black      "\033[7;37;40m"
+
+#define yellow     "\033[7;33;44m"
+
+#define foreground "\033[38;5;154m"
+
+#define background "\033[38;5;0;48;5;154m"
+
+int main()
+{
+	cout << black      << "[ Black on White ]"           << default << endl;
+
+	cout << endl;
+	cout << endl;
+
+	cout << yellow     << "[ Intense Yellow with Blue ]" << default << endl;
+	
+	cout << endl;
+	cout << endl;
+
+	cout << green	   << "[ Normal Green ]"			 << default << endl;
+	
+	cout << endl;
+	cout << endl;
+
+	cout << red        << "[ Underlined Red ]"			 << default << endl;
+	
+	cout << endl;
+	cout << endl;
+
+	cout << foreground << "[ 256 Colors ]"				 << default << endl;
+	
+	cout << endl;
+	cout << endl;
+
+	cout << background << "[ 256 Colors ]"				 << default << endl;
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
