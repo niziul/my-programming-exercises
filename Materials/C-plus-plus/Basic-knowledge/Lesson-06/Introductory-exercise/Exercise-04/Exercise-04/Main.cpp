5cda88972ff6525566766623034dd5ab27a6c059
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-03-05 22:08:10 -0300

    [C++] - Basic Knowledge - Commit - 83
    Lesson - 06 - Integer Types, Variables, Constants, Overflow and Underflow
    
    - Fourth Introductory Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-06/Introductory-exercise/Exercise-04/Exercise-04/Main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <climits>
+
+#define ZERO 0;
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	short amount_of_money_pedro_has
+		= ZERO;
+	unsigned short amount_of_money_maria_has
+		= ZERO;
+
+	cout << "------------------------------- --------------- -------- ---- --- -- -" << endl;
+	cout << "[ Pedro have " << amount_of_money_pedro_has << " dollars. ]" << endl;
+	cout << "------------------------------- --------------- -------- ---- --- -- -" << endl;
+	cout << "[ Maria have " << amount_of_money_maria_has << " dollars. ]" << endl;
+	cout << "------------------------------- --------------- -------- ---- --- -- -" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	cout << "] ------------------------------ Taking 1 dollar each..." << endl;
+
+	amount_of_money_pedro_has -= 1;
+	amount_of_money_maria_has -= 1;
+
+	cout << endl;
+	cout << endl;
+
+	cout << "------------------------------- --------------- -------- ---- --- -- -" << endl;
+	cout << "[ Now Pedro have " << amount_of_money_pedro_has << " dollars. ]" << endl;
+	cout << "------------------------------- --------------- -------- ---- --- -- -" << endl;
+	cout << "[ Now Maria have " << amount_of_money_maria_has << " dollars. ]" << endl;
+	cout << "------------------------------- --------------- -------- ---- --- -- -" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
