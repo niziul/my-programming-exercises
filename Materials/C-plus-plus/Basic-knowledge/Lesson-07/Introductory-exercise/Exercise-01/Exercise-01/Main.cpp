b31f9a01836c2527b1da95aec4ce4e80cbece1f4
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-03-11 20:42:27 -0300

    [C++] - Basic Knowledge - Commit - 95
    Lesson - 07 - Character Types, Boolean, ASCII Table and Bitwise Operators
    
    - First Introductory Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-07/Introductory-exercise/Exercise-01/Exercise-01/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	char character;
+
+	cout << "Enter a character" << endl;
+	cout << ": ";
+	cin >> character;
+
+	cout << endl;
+	cout << endl;
+
+	cout << "[ Hello! Thanks for the character: '" << character << "' ]" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
