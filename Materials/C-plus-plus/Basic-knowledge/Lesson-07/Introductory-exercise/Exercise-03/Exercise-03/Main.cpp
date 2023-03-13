a613abbc7e5ab4bdc9a787bc47a441609497816b
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-03-12 22:28:17 -0300

    [C++] - Basic Knowledge - Commit - 97
    Lesson - 07 - Character Types, Boolean, ASCII Table and Bitwise Operators
    
    - Third Introductory Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-07/Introductory-exercise/Exercise-03/Exercise-03/Main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	char char_clock = '\a';
+	int int_password;
+
+	cout << "Enter your password: _________ \b\b\b\b\b\b\b\b\b\b";
+	cin >> int_password;
+
+	cout << endl;
+	cout << endl;
+
+	cout << char_clock << "Your password has been stolen!\a" << endl;
+	
+	cout << endl;
+	cout << endl;
+
+	cout << "jj the \"hack\" was here!" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
