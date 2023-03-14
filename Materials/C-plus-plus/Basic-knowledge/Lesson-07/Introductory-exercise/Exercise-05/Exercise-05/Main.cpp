dce069a1aa6eb2e1bd58d81fa776b85e720490db
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-03-14 19:32:56 -0300

    [C++] - Basic Knowledge - Commit - 99
    Lesson - 07 - Character Types, Boolean, ASCII Table and Bitwise Operators
    
    - Fifth Introductory Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-07/Introductory-exercise/Exercise-05/Exercise-05/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	unsigned char state = 1;
+
+	state = ~state;
+
+	cout << "state content : [ " << (int16_t)state << " ]" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
