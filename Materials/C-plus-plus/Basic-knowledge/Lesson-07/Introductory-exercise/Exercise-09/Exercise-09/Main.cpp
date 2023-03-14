d0e86e139f4441a466bf17184ec4b2fb17e63215
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-03-14 20:08:55 -0300

    [C++] - Basic Knowledge - Commit - 103
    Lesson - 07 - Character Types, Boolean, ASCII Table and Bitwise Operators
    
    - Ninth Introductory Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-07/Introductory-exercise/Exercise-09/Exercise-09/Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	unsigned char state = 170;
+
+	state = state | 15;
+
+	cout << "------------------------ ------------ ------ ---" << endl;
+	cout << "lamps on: 170 bit sequence : [ 1 0 1 0 1 0 1 0 ]" << endl;
+	cout << "------------------------ ------------ ------ ---" << endl;
+	cout << "lamps on: 15  bit sequence : [ 0 0 0 0 1 1 1 1 ]" << endl;
+	cout << "------------------------ ------------ ------ ---" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	cout << "lamps on: " << (int16_t)state << " bit sequence : [ 1 0 1 0 1 1 1 1 ]" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
