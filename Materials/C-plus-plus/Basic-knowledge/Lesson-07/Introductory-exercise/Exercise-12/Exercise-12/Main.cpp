f5b7000b0f597c14dbdf55ddc8d3968a96c1fa9a
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-03-15 21:24:37 -0300

    [C++] - Basic Knowledge - Commit - 106
    Lesson - 07 - Character Types, Boolean, ASCII Table and Bitwise Operators
    
    - Twelfth Introductory Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-07/Introductory-exercise/Exercise-12/Exercise-12/Main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	unsigned char bit_mask;
+	unsigned char bit_state;
+	unsigned char bit_that_the_user_wants_to_turn_on;
+
+	cout << endl;
+	cout << endl;
+
+	cout << "Do you want to turn off which bit ( ex: 3 )" << endl;
+	cout << ": ";
+	cin >> bit_that_the_user_wants_to_turn_on;
+
+	cout << endl;
+	cout << endl;
+
+	bit_mask = ~(1 << bit_that_the_user_wants_to_turn_on);
+
+	bit_state = 252 & bit_mask;
+
+	cout << "Pattern of the bits that represent the lamp status:" << endl;
+
+	cout << "------------------------ ------------ ------ --- -- -" << endl;
+	cout << "\"" << (int16_t)bit_state << "\" [ 1 1 1 1 0 1 0 0 ] ( 1 = lamp on | 0 = lamp off )" << endl;
+	cout << "------------------------ ------------ ------ --- -- -" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
