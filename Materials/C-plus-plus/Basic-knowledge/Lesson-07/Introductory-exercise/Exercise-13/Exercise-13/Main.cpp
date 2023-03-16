4085eaae83c3a41a9d47ac1426566a453e21aad4
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-03-15 21:43:08 -0300

    [C++] - Basic Knowledge - Commit - 107
    Lesson - 07 - Character Types, Boolean, ASCII Table and Bitwise Operators
    
    - Thirteenth Introductory Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-07/Introductory-exercise/Exercise-13/Exercise-13/Main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	unsigned bit_mask; 
+	unsigned bit_state; 
+	unsigned bit_that_the_user_wants_to_testand; // refers to the state of whether a lamp is_on
+	
+	cout << endl;
+	cout << endl;
+
+	cout << "Which bit do you want to test ( ex: 7 )" << endl;
+	cout << ": ";
+	cin >> bit_that_the_user_wants_to_testand;
+
+	cout << endl;
+	cout << endl;
+
+	bit_mask = 1 << bit_that_the_user_wants_to_testand;
+
+	bit_state = 240;
+
+	if (bit_state & bit_mask)
+	{
+		cout << "------------------------ ------------ ------ --- -- -" << endl;
+		cout << "--] is turned on!" << endl;
+		cout << "------------------------ ------------ ------ --- -- -" << endl;
+	}
+	else
+	{
+		cout << "------------------------ ------------ ------ --- -- -" << endl;
+		cout << "--] is not turned on!!" << endl;
+		cout << "------------------------ ------------ ------ --- -- -" << endl;
+	}
+	
+	cout << endl;
+	cout << endl;
+	
+	system("pause");
+}
