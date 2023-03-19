dc092b781a993abe14de3552d3e250dc4960a09d
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-03-19 00:02:09 -0300

    [C++] - Basic Knowledge - Commit - 111
    Lesson - 07 - Character Types, Boolean, ASCII Table and Bitwise Operators
    
    - Fourth Fixation Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-07/Fixation-exercises/Exercise-04/Exercise-04/Main.cpp
@@ -0,0 +1,50 @@
+/*
+	[ description of the fourth fixation exercise ] :
+		- Write a program that uses the cout statement along with the
+		  tab character to produce the following output:
+
+		  ]
+			```
+				plum =		 30 unit
+				apple =		 20 unit
+				watermelon = 50 unit
+			```
+		  ]
+
+	[ output ] :
+		```
+			plum =       30 unit
+			apple =      20 unit
+			watermelon = 50 unit
+		```
+*/
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "plum =";
+	for (int16_t i = 0; i < 7; i++)
+	{
+		cout << '\x20';
+	}
+	cout << "30 unit" << endl;
+
+	cout << "apple =";
+	for (int16_t i = 0; i < 6; i++)
+	{
+		cout << '\x20';
+	}
+	cout << "20 unit" << endl;
+
+	cout << "watermelon =" << " 50 unit" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
