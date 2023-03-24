06c9b553158744ff0bb3031f23838f6ba0764322
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-03-24 20:59:06 -0300

    [C++] - Basic Knowledge - Commit - 117
    Lesson - 07 - Character Types, Boolean, ASCII Table and Bitwise Operators
    
    - Fourth Learning Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-07/Learning-exercises/Exercise-04/Exercise-04/Main.cpp
@@ -0,0 +1,65 @@
+/*
+	[ description of the fourth learning exercise ] :
+		- Build a function called high_bits(); which uses a bitmask to return only the high-order 16 bits of an
+		  only the high-order 16 bits of a number of type 'int'. Test the function by passing an integer
+		  value entered by the user and show the number returned.
+
+	[ output ] :
+		```
+
+		:::::::::::::::::::: :::::::::: ::::: ::: :
+		--] High Bits
+		:::::::::::::::::::: :::::::::: ::::: ::: :
+
+
+		Enter an integer value
+		: 350000
+
+
+		The higher 16 bits of this value '350000' correspond to the number: '5'
+		```
+*/
+
+#include<iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int high_bits(int);
+
+int main()
+{
+	int user_value;
+
+	cout << endl;
+	cout << endl;
+
+	cout << ":::::::::::::::::::: :::::::::: ::::: ::: :" << endl;
+	cout << "--] High Bits" << endl;
+	cout << ":::::::::::::::::::: :::::::::: ::::: ::: :" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	cout << "Enter an integer value" << endl;
+	cout << ": ";
+	cin >> user_value;
+
+	cout << endl;
+	cout << endl;
+
+	cout << "The higher 16 bits of this value \'" << user_value << "\' correspond to the number: \'" << high_bits(user_value) << "\'" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
+
+int high_bits(int user_value)
+{
+	auto mask = ~32767i16;
+
+	return (user_value & mask) >> 16;
+}
