48a3408ea680c67940934d3b3a875f341f57ae33
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-03-24 21:52:40 -0300

    [C++] - Basic Knowledge - Commit - 118
    Lesson - 07 - Character Types, Boolean, ASCII Table and Bitwise Operators
    
    - Fifth Learning Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-07/Learning-exercises/Exercise-05/Exercise-05/Main.cpp
@@ -0,0 +1,76 @@
+/*
+	[ description of the fifth learning exercise ] :
+		- Build a function called display_bits() that, given an unsigned char value, displays on the screen the
+		  binary representation of that number. The function display_bits() should use the function test_bits()
+		  ( from fixation exercise 5 ) to find out which bits of the received value are equal to 0 and which
+		  are equal to 1.
+
+	[ output ] :
+		```
+			------------------------ ------------ ------ --- -- -
+			--] Display Bits
+			------------------------ ------------ ------ --- -- -
+
+
+			Digite um valor entre 0 e 255
+			: 143
+
+
+			The value '143' corresponds to this sequence in binary:
+			[ 10001111 ]
+		```
+*/
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void display_bits(unsigned char);
+unsigned char test_bits(unsigned char, int16_t);
+
+int main()
+{
+	int16_t user_value;
+
+	cout << endl;
+	cout << endl;
+
+	cout << "------------------------ ------------ ------ --- -- -" << endl;
+	cout << "--] Display Bits" << endl;
+	cout << "------------------------ ------------ ------ --- -- -" << endl;
+	
+	cout << endl;
+	cout << endl;
+
+	cout << "Digite um valor entre 0 e 255" << endl;
+	cout << ": ";
+	cin >> user_value;
+
+	cout << endl;
+	cout << endl;
+
+	cout << "The value \'" << user_value << "\' corresponds to this sequence in binary:" << endl;
+	cout << "[ ";
+	display_bits(user_value);
+	cout << " ]";
+	
+	cout << endl;
+	cout << endl;
+
+	system("pause");
+}
+
+void display_bits(unsigned char char_value)
+{
+	for (int16_t i = 7; i >= 0; i--)
+	{
+		cout << (int16_t)test_bits(char_value, i);
+	}
+}
+
+unsigned char test_bits(unsigned char char_value, int16_t bit_index)
+{
+	return char_value & 1 << bit_index ? 1 : 0;
+}
