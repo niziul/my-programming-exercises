620f566b6d8fc0c9f63fbe22834876200ce62d6a
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-04-15 23:16:36 -0300

    [C++] - Basic Knowledge
    Lesson - 09 - Arithmetic Operators, Type Conversions and Type Cast
    
    - Second Introductory Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-09/Introductory-exercise/Exercise-02/Exercise-02/Main.cpp
@@ -0,0 +1,19 @@
+#define nextline '\n'
+
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+	auto k = -1;
+	auto x = -k;
+
+	cout << nextline;
+
+	cout << x << nextline;
+
+	cout << nextline;
+
+	system("pause");
+}
