694bf7d86846b609b15f89777139fc8bc5b5f450
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-04-20 23:38:22 -0300

    [C++] - Basic Knowledge
    Lesson - 09 - Arithmetic Operators, Type Conversions and Type Cast
    
    - Fifth Introductory Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-09/Introductory-exercise/Exercise-05/Exercise-05/Main.cpp
@@ -0,0 +1,70 @@
+/*
+	[ output ] :
+		```
+			------=-- --=----=-- --=----=-- --=------
+			Enter a min value
+			--
+			> 10
+
+			Enter a max value
+			--
+			> 20
+
+			------=-- --=----=-- --=----=-- --=------
+			Randomizing a value between '10' and '20'
+			--] '18'
+			------=-- --=----=-- --=----=-- --=------
+		```
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+	auto nextline
+         = '\n';
+
+	auto random_number
+         = 0;
+
+	auto min
+         = 0;
+
+	auto max
+         = 0;
+
+	cout << "------=-- --=----=-- --=----=-- --=------" << nextline;
+
+	cout << "Enter a min value" << nextline;
+	cout << "--" << nextline;
+	cout << "> ";
+	cin >> min;
+
+	cout << nextline;
+
+	cout << "Enter a max value" << nextline;
+	cout << "--" << nextline;
+	cout << "> ";
+	cin >> max;
+
+	cout << nextline;
+
+	cout << "------=-- --=----=-- --=----=-- --=------" << nextline;
+	
+	cout << "Randomizing a value between \'" << min << "\' and \'" << max << "\'" << nextline;
+	
+	random_number
+         = min + rand() % (max - min + 1);
+
+	cout << "--] \'" << random_number << "\'" << nextline;
+
+	cout << "------=-- --=----=-- --=----=-- --=------" << nextline;
+
+	cout << nextline;
+	
+	system("pause");
+}
