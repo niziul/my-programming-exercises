7127fdf36c5c947c14330c7a4c2a96b3c8fe9505
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-06-06 18:52:03 -0300

    Add description and output to the 1° - Fixation Exercise

--- Materials/C-plus-plus/Basic-knowledge/Lesson-03/Fixation-exercises/Exercise-01/Exercise-01/Main.cpp
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-03/Fixation-exercises/Exercise-01/Exercise-01/Main.cpp
@@ -1,3 +1,49 @@
+/*
+	[ description of the first fixation exercise ] :
+		- Correct and run the program below:
+
+		]
+			#include <iostream>
+
+			using std::cin;
+			using std::cout;
+			using std::endl;
+
+			int main()
+			{
+				int seconds, int minutes;
+
+				cout << "Enter a number of minutes:
+				cin >> minutes
+
+				cout << endl;
+
+				seconds = minutes * 100;
+
+				cout << There is a total of  << seconds << " seconds in " 
+					<< minutes <<  minute!;
+
+				Cout << endl;
+
+				system("pausa");
+				return null;
+			}
+		]
+
+		If the user enters 2, the program should display:
+
+		]
+			There is a total of 120 seconds in 2 minute!
+		]
+
+	[ output ] :
+		```
+			Enter a number of minutes: 2
+
+			There is a total of 120 seconds in 2 minute!
+		```
+*/
+
 #include <iostream>
 
 using std::cin;
