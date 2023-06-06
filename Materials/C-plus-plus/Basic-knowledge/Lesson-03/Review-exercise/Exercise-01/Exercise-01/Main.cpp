044515d0caf5b9d902be44743c86695e9185ca3a
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-06-06 15:15:43 -0300

    Add description and output to the 1° - Review Exercise

--- Materials/C-plus-plus/Basic-knowledge/Lesson-03/Review-exercise/Exercise-01/Exercise-01/Main.cpp
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-03/Review-exercise/Exercise-01/Exercise-01/Main.cpp
@@ -1,3 +1,43 @@
+/*
+	[ description of the first review exercise ] :
+		- Use only one 'cin' statement to read the following three pieces of information
+		for different variables. Multiply the values of the variables height, width
+		and length to find the volume of the cube.
+		
+		]
+			Enter the height, width and length:
+			2.1 3.5 5.0
+			
+			The volume is 36.75 cubic cm.
+		]
+
+		Does the program work if you press 'Enter' instead of 'Space' after typing
+		each number?
+
+		Answer: yes!
+
+		]
+			Enter the height, width and length:
+			2.1
+			3.5
+			5.0
+			
+			The volume is 36.75 cubic cm.
+		]
+
+		What if we use a cin statement for each number? Is it still possible to enter all the
+		numbers on the same line?
+
+		Answer: yes!
+
+	[ output ] :
+		```
+			Enter the height, width and length: 2.1 3.5 5.0
+
+			The volume is 36.75 cubic cm.
+		```
+*/
+
 #include <iostream>
 
 using std::cin;
@@ -20,7 +60,7 @@ int main()
 
 	float volume = height * width * length;
 
-	cout << "volume = " << volume;
+	cout << "The volume is = " << volume << " cubic cm";
 
 	cout << endl;
 
