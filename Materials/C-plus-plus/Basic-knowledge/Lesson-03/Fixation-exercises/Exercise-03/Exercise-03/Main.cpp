50b8465d631083f7ae578c161b00a31e6213c20a
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-06-06 19:10:13 -0300

    Add description and output to the 3° - Fixation Exercise

--- Materials/C-plus-plus/Basic-knowledge/Lesson-03/Fixation-exercises/Exercise-03/Exercise-03/Main.cpp
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-03/Fixation-exercises/Exercise-03/Exercise-03/Main.cpp
@@ -1,3 +1,22 @@
+/*
+	[ description of the third fixation exercise ] :
+		- Write a program that reads your age in years and converts it to days. The program
+		should display both values on the screen, as in the example below:
+
+		]
+			Enter your age in years: 20
+
+			20 year are equivalent to 7300 days.
+		]
+
+	[ output ] :
+		```
+			Enter your age in years: 20
+
+			20 year are equivalent to 7300 days.
+		```
+*/
+
 #include <iostream>
 
 using std::cin;
