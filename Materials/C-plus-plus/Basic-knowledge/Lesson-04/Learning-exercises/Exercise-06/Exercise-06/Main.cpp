b40775870f8abc5a04896d5e8ba90ab16bb6c081
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-06-07 20:13:55 -0300

    Fix description of the 6° - Learning Exercise

--- Materials/C-plus-plus/Basic-knowledge/Lesson-04/Learning-exercises/Exercise-06/Exercise-06/Main.cpp
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-04/Learning-exercises/Exercise-06/Exercise-06/Main.cpp
@@ -1,17 +1,16 @@
 /*
 	[ description of the sixth learning exercise ] :
 		- Write an initialize() function that uses four other functions to accomplish its task.
-		  the function must write "Initializing system: ",
-		  call the power(), check() and activate() functions, and then write "Initialization
-		  completed". To simulate the result of the initialization, the function initialize() should
-		  return a random integer value, obtained by calling the rand() function.
+		  the function must write "Initializing system: ", call the power(), check() and activate()
+		  functions, and then write "Initialization completed". To simulate the result of the initialization,
+		  the function initialize() should return a random integer value, obtained by calling the rand() function.
+		  
+		  The rand() function belongs to the <cstdlib> library and returns a random number between 0 and 32767
+		  each time it is called. The functions call, check() and activate() functions should be created to display
+		  messages, as in the example below.
 
-		  The rand() function belongs to the <cstdlib> library and returns a random number
-		  between 0 and 32767 each time it is called. The functions call, check() and activate()
-		  functions should be created to display messages, as in the example below.
-
-		  If the result returned by initialize() is greater than 16384, the program
-		  should display "System is running", otherwise it should display: "Initialization failed".
+		  If the result returned by initialize() is greater than 16384, the program should display "System is running",
+		  otherwise it should display: "Initialization failed".
 
 	[ output ] :
 		```
