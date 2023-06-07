bc62d9e52d9513c2fceb86340e25c7e1ba49dbc8
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-06-07 20:02:55 -0300

    Fix description of the 2° - Fixation Exercise

--- Materials/C-plus-plus/Basic-knowledge/Lesson-04/Fixation-exercises/Exercise-02/Exercise-02/Main.cpp
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-04/Fixation-exercises/Exercise-02/Exercise-02/Main.cpp
@@ -1,12 +1,11 @@
 /*
 	[ description of the second fixation exercise ] :
 		- Write a Line() function that, using a single cout and without using repeats or
-		  jumps, produces a fixed length line consisting of 10 hyphen characters.
-		  Also write a Small(), Medium(), and Large() function that makes use of
-		  of the Line() function and line skips to produce lines with 10, 20 and 30 hyphens
-		  respectively. The program should produce the output below. The Line() function must not
-		  must not be called directly from main(), only Small(), Medium() and Large() must
-		  Large() should make use of it.
+		  jumps, produces a fixed length line consisting of 10 hyphen characters. Also write
+		  a Small(), Medium(), and Large() function that makes use of the Line() function and line
+		  skips to produce lines with 10, 20 and 30 hyphens respectively. The program should produce the
+		  output below. The Line() function must not be called directly from main(), only Small(), Medium() and Large()
+		  must Large() should make use of it.
 	
 	[ output ] :
 		```
