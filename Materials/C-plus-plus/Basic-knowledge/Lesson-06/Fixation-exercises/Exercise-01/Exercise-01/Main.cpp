2cac1e58c2a9e177f7bb7c71e7c6432fb150e215
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-06-09 16:18:26 -0300

    Add description and output to the 1° - Fixation Exercise

--- Materials/C-plus-plus/Basic-knowledge/Lesson-06/Fixation-exercises/Exercise-01/Exercise-01/Main.cpp
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-06/Fixation-exercises/Exercise-01/Exercise-01/Main.cpp
@@ -1,3 +1,43 @@
+/*
+	[ description of the first fixation exercise ] :
+		- Correct the program below by keeping the symbolic constant 'Zero'.
+
+		]
+				include iostream
+				using std::cout
+				using std::endl
+				int main{}
+				(
+					Int total;
+
+					Cout << "Initially total = " << Total << " (ZERO)\n";
+
+					Total = 50;
+
+					cout << "After total assignment = " << total << endl;
+
+					Return ZERO;
+				)
+		]
+
+		So that the output will be:
+
+		]
+			Initially total = 0 (ZERO)
+
+
+			After total "assignment" = 50
+		]
+
+	[ output ] :
+		```
+			Initially total = 0 (ZERO)
+
+
+			After total "assignment" = 50
+		```
+*/
+
 #include <iostream>
 
 using std::cout;
