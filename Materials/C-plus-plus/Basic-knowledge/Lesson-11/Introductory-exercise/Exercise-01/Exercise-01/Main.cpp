4f0ef3359b09ccd2d5cf0107053eefc8ed5a9c39
niziul <NiziulLuizin@Gmail.com>
2023-06-13 22:31:58 -0300

    [C++] - Basic Knowledge
    Lesson - 11 - Strings, Vector of Chars and String Type
    
    - First Introductory Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-11/Introductory-exercise/Exercise-01/Exercise-01/Main.cpp
@@ -0,0 +1,59 @@
+﻿/*
+	[ output ] :
+		```
+			<.> - - - - - - - - - - - - - - - - <.>
+
+			--] meow
+
+			--] bark╠╠╠╠╠╠╠╠╠╠╠╠╠╠╠╠╠╠╠╠╠p√∩╜▄
+
+			<.> - - - - - - - - - - - - - - - - <.>
+		```
+*/
+
+#include <iostream>
+#include <array>
+
+using std::cout;
+using std::endl;
+using std::array;
+
+int main()
+{
+	auto cat
+		= array<char, 5>
+		{
+			'm',
+			'e',
+			'o',
+			'w',
+			'\0'
+		};
+
+	auto dog
+		= array<char, 4>
+		{
+			'b',
+			'a',
+			'r',
+			'k'
+		};
+
+	cout << "<.> - - - - - - - - - - - - - - - - <.>" << endl;
+
+	cout << endl;
+
+	cout << "--] ";
+	cout << cat.data() << endl;
+
+	cout << endl;
+	
+	cout << "--] ";
+	cout << dog.data() << endl;
+
+	cout << endl;
+	
+	cout << "<.> - - - - - - - - - - - - - - - - <.>" << endl;
+
+	system("pause");
+}
