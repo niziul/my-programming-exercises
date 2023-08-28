ed74ef4c60f9d24010160d2bbfbfa9d9d09b27da
niziul <NiziulLuizin@Gmail.com>
2023-08-28 12:18:52 -0300

    [C++] - Basic Knowledge
    Lesson - 18 - While and Do-While Loops, Comparing Strings and Floating-Point
    
    - Third Learning Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-18/Learning-exercises/Exercise-03/Exercise-03/Main.cpp
@@ -0,0 +1,82 @@
+/*
+	[ description of the third learning exercise ] :
+		- Build a program that asks the user to type a text, ending the input only
+		when the '@' character is read. After '@' is read, display the text received.
+
+		]
+			Enter a text:
+
+			I sent a message to @joaozinho about this problem.
+
+			I sent a message to
+		]
+
+	[ output ] :
+		```
+			<@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>
+
+			Enter a text below
+			- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+			: I sent a message to @joaozinho about this problem.
+
+			-
+
+			- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+			 - - ] I sent a message to ;
+
+			- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+			<@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>
+		```
+*/
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	auto text
+		= new char[80];
+
+	cout << "<@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>" << endl;
+
+	cout << endl;
+
+	cout << "Enter a text below" << endl;
+	cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
+	cout << ": ";
+	cin.getline(text, 80);
+
+	cout << endl;
+	cout << "-" << endl;
+	cout << endl;
+
+	cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
+	
+	cout << endl;
+	
+	cout << "- - ] ";
+
+	for (auto i = 0; *(text + i) != '@'; i++)
+	{
+		cout << *(text + i);
+	}
+
+	cout << ";";
+	cout << endl;
+	cout << endl;
+	
+	cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
+	
+	cout << endl;
+	
+	cout << "<@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>" << endl;
+
+	delete[] text;
+
+	system("pause");
+}
