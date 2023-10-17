619179c8933b8ad1bfc393ec01ecf5ffa64bb22e
niziul <NiziulLuizin@Gmail.com>
2023-10-17 18:04:35 -0300

    [C++] - Basic Knowledge
    Lesson - 21 - Logical Operators and Conditional Testing
    
    - Second Review Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-21/Review-exercise/Exercise-02/Exercise-02/Main.cpp
@@ -0,0 +1,44 @@
+/*
+	[ description of the second review exercise ] :
+		- In c++, what is the result of negating an expression twice? That
+		is, '!!x' is equal to 'x'?
+
+	[ output ] :
+		```
+			<@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>
+
+			] 0;
+
+			<@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>
+		```
+*/
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+using std::streamsize;
+using std::numeric_limits;
+
+int main()
+{
+	auto value_x
+		= 23;
+
+	cout << "<@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>" << endl;
+
+	cout << endl;
+
+	cout << "] " << (!!value_x == value_x) << ";" << endl;
+
+	cout << endl;
+
+	cout << "<@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>" << endl;
+
+	cin
+		.ignore(numeric_limits<streamsize>::max(), '\n');
+	cin
+		.get();
+}
