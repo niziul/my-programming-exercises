a6df6f924fa5de2f73c69e2a1b2eef7a6ca81d34
niziul <NiziulLuizin@Gmail.com>
2024-05-01 13:08:03 -0300

    [C++] - Basic Knowledge
    Lesson - 28 - Storage, References, Scope and External / Internal Linking
    
    - First Introductory Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-28/Introductory-exercise/Exercise-01/Exercise-01/Main.cpp
@@ -0,0 +1,48 @@
+/*
+    [ output ] :
+        ```
+            <@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>
+
+            ] size
+            - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            - '5000'
+
+            <@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>
+        ```
+*/
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+using std::streamsize;
+using std::numeric_limits;
+
+int size = 1000;
+
+int calculateSize(int);
+
+int main()
+{
+
+    cout << "<@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>" << endl;
+
+    cout << endl;
+
+    cout << "] size" << endl;
+    cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
+
+    cout << endl;
+
+    cout << "- \'" << calculateSize(5) << "\'" << endl;
+
+    cout << endl;
+
+    cout << "<@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>" << endl;
+
+    cin
+        .ignore(numeric_limits<streamsize>::max(), '\n');
+}
---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-28/Introductory-exercise/Exercise-01/Exercise-01/Utility.cpp
@@ -0,0 +1,6 @@
+extern int size;
+
+int calculateSize(int value)
+{
+    return size * value;
+}
