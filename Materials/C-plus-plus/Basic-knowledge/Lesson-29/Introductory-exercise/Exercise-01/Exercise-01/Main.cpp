6abbb7223592c0dfd3a63b5473749bc0df9cef44
niziul <NiziulLuizin@Gmail.com>
2024-05-16 12:35:52 -0300

    [C++] - Basic Knowledge
    Lesson - 29 - Function Templates, Function Overloading and Default Arguments
    
    - First Introductory Exercise

---
+++ Materials/C-plus-plus/Basic-knowledge/Lesson-29/Introductory-exercise/Exercise-01/Exercise-01/Main.cpp
@@ -0,0 +1,51 @@
+/*
+    [ output ] :
+        ```
+            <@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>
+
+            ---------------------------***********---------------------------
+
+            <@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>
+        ```
+*/
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+using std::streamsize;
+using std::numeric_limits;
+
+void line(int = 10, char = '-');
+
+int main()
+{
+    cout << "<@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>" << endl;
+
+    cout << endl;
+
+    line();
+    line(15);
+    line(10, '*');
+    line(15);
+    line();
+    //line('*'); // bug
+
+    cout << endl;
+    cout << endl;
+
+    cout << "<@> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <@>" << endl;
+
+    cin
+        .ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+void line(int size, char character)
+{
+    do
+    {
+        cout << character;
+    } while (size-- != 0);
+}
