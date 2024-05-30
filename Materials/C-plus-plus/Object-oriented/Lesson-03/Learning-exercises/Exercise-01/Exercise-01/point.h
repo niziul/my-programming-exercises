8eed4867c045f0bf76ad969ceaad2f7d002f22f4
niziul <NiziulLuizin@Gmail.com>
2024-05-30 15:35:03 -0300

    [C++] - Object Oriented
    Lesson - 03 - Objects and Instances
    
    - Fix First Learning Exercise

--- Materials/C-plus-plus/Object-oriented/Lesson-03/Learning-exercises/Exercise-01/Exercise-01/point.h
+++ Materials/C-plus-plus/Object-oriented/Lesson-03/Learning-exercises/Exercise-01/Exercise-01/point.h
@@ -5,31 +5,31 @@
 class c_point
 {
 public:
-	c_point(int_fast16_t width, int_fast16_t height)
-	{
-		this->x
-			= 0;
-		this->y
-			= 0;
+    c_point(int_fast16_t width, int_fast16_t height)
+    {
+        this->x
+            = 0;
+        this->y
+            = 0;
 
-		this->width
-			= width;
-		this->height
-			= height;
-	}
+        this->width
+            = width;
+        this->height
+            = height;
+    }
 
-	int_fast16_t get_x() const;
-	int_fast16_t get_y() const;
+    int_fast16_t get_x() const;
+    int_fast16_t get_y() const;
 
-	int_fast16_t get_width() const;
-	int_fast16_t get_height() const;
+    int_fast16_t get_width() const;
+    int_fast16_t get_height() const;
 
-	void move_to(int_fast16_t, int_fast16_t);
-	void translate(int_fast16_t, int_fast16_t);
+    void move_to(int_fast16_t, int_fast16_t);
+    void translate(int_fast16_t, int_fast16_t);
 
 private:
-	int_fast16_t width;
-	int_fast16_t height;
-	int_fast16_t x;
-	int_fast16_t y;
+    int_fast16_t width;
+    int_fast16_t height;
+    int_fast16_t x;
+    int_fast16_t y;
 };
