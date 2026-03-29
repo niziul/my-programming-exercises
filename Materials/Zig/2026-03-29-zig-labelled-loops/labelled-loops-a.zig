00223b6845ea407415ec9c6638e6a2b3af0fa217
niziul <niziulluizin@gmail.com>
2026-03-29 11:28:57 -0300

    [Zig]
    add the zig labelled loops example

---
+++ Materials/Zig/2026-03-29-zig-labelled-loops/labelled-loops-a.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+
+test "nested continue" {
+    var count: usize = 0;
+
+    outer: for ([_]i32{
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+    }) |_| {
+        for ([_]i32{
+            1,
+            2,
+            3,
+            4,
+            5,
+        }) |_| {
+            count += 1;
+            continue :outer;
+        }
+    }
+
+    try std.testing.expect(count == 8);
+}
