894b40c2cff0bc67c86a14f9868bd607060e1a29
niziul <niziulluizin@gmail.com>
2026-04-16 10:17:35 -0300

    [Zig]
    add the zig inline loops example

---
+++ Materials/Zig/2026-04-16-zig-inline-loops/inline-loops-a.zig
@@ -0,0 +1,16 @@
+const expect = @import("std").testing.expect;
+
+test "inline for" {
+    const types = [_]type{
+        i32,
+        f32,
+        u8,
+        bool,
+    };
+
+    var sum: usize = 0;
+
+    inline for (types) |T| sum += @sizeOf(T);
+
+    try expect(sum == 10);
+}
